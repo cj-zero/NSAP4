4c759465716c7e8a66b6f251371008815df79d30
饭勺oo <liujunfan@neware.com.cn>
2020-07-17 14:37:35 +0800

    网关添加日志

--- OpenAuth.Core/ApiGateway/Program.cs
+++ OpenAuth.Core/ApiGateway/Program.cs
@@ -1,18 +1,35 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace ApiGateway
 {
     public class Program
     {
         public static void Main(string[] args)
-        {
+        {       
+            // 配置 Serilog 
+            Log.Logger = new LoggerConfiguration()
+                // 最小的日志输出级别
+                .MinimumLevel.Information()
+                // 日志调用类命名空间如果以 Microsoft 开头，覆盖日志输出最小级别为 Information
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                // 配置日志输出到控制台
+                .WriteTo.Console()
+                // 配置日志输出到文件，文件输出到当前项目的 logs 目录下
+                // 日记的生成周期为每天
+                .WriteTo.File(Path.Combine("logs", @"log.txt"), rollingInterval: RollingInterval.Day)
+                // 创建 logger
+                .CreateLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -20,7 +37,9 @@ namespace ApiGateway
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    // 将 Serilog 设置为日志记录提供程序
+                    .UseSerilog(); ;
                 });
     }
 }
