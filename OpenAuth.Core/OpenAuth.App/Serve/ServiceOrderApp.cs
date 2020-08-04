a892de56a8955ac745e79c43ab8ffee3701ce7b1
刘俊繁 <liujunfan@neware.com.cn>
2020-08-04 16:26:16 +0800

    消息

--- OpenAuth.Core/OpenAuth.App/Serve/ServiceOrderApp.cs
+++ OpenAuth.Core/OpenAuth.App/Serve/ServiceOrderApp.cs
@@ -688,7 +688,7 @@ namespace OpenAuth.App
                 .WhereIf(!string.IsNullOrWhiteSpace(req.QryProblemType), q => q.ServiceWorkOrders.Any(a => a.ProblemTypeId.Equals(req.QryProblemType)))
                 .WhereIf(!(req.QryCreateTimeFrom is null || req.QryCreateTimeTo is null), q => q.ServiceWorkOrders.Any(a => a.CreateTime >= req.QryCreateTimeFrom && a.CreateTime <= req.QryCreateTimeTo));
 
-            var resultsql = query.Select(q => new
+            var resultsql = query.OrderByDescending(q => q.CreateTime).Select(q => new
             {
                 ServiceOrderId = q.Id,
                 q.CustomerId,
@@ -706,7 +706,7 @@ namespace OpenAuth.App
                 && (string.IsNullOrWhiteSpace(req.QryState) || a.Status.Equals(Convert.ToInt32(req.QryState)))
                 && (string.IsNullOrWhiteSpace(req.QryManufSN) || a.ManufacturerSerialNumber.Contains(req.QryManufSN))
                 && ((req.QryCreateTimeFrom == null || req.QryCreateTimeTo == null) || (a.CreateTime >= req.QryCreateTimeFrom && a.CreateTime <= req.QryCreateTimeTo))
-                ).ToList()
+                  ).ToList()
             });
 
             result.Data = await resultsql.Skip((req.page - 1) * req.limit)
