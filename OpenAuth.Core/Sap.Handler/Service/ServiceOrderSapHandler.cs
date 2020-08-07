25379904a27b281727d7bf0f81dde6a65e975e82
刘俊繁 <liujunfan@neware.com.cn>
2020-08-07 15:10:34 +0800

    SAP处理模块

---
+++ OpenAuth.Core/Sap.Handler/Service/ServiceOrderSapHandler.cs
@@ -0,0 +1,25 @@
+﻿using DotNetCore.CAP;
+using OpenAuth.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sap.Handler.Service
+{
+    public class ServiceOrderSapHandler : ICapSubscribe
+    {
+        private readonly IUnitWork UnitWrok;
+
+        public ServiceOrderSapHandler(IUnitWork unitWrok)
+        {
+            UnitWrok = unitWrok;
+        }
+
+        [CapSubscribe("Serve.ServcieOrder.Create")]
+        public async Task HandleServiceOrder(int id)
+        {
+
+        }
+    }
+}
---
+++ OpenAuth.Core/Sap.Handler/Startup.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Neware.Cap.DependencyInjection;
+using OpenAuth.Repository.Extensions;
+using Sap.Handler.DependencyInjection;
+using Sap.Handler.Service;
+
+namespace Sap.Handler
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //≈‰÷√autoMapper
+            services.AddAutoMapper();
+
+            services.AddDbContexts();
+
+            services.AddSap(Configuration);
+
+            services.AddNewareCAP(Configuration);
+
+            services.AddSingleton<ServiceOrderSapHandler>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            // ≈‰÷√æ≤Ã¨autoMapper
+            AutoMapperHelper.UseStateAutoMapper(app);
+
+            app.UseRouting();
+        }
+    }
+}
