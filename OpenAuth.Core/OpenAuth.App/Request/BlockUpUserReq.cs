f6bde5842c8eb123e7c6937a50c6b40574b9c734
刘俊繁 <liujunfan@neware.com.cn>
2020-08-10 15:18:44 +0800

    bug fix

---
+++ OpenAuth.Core/OpenAuth.App/Request/BlockUpUserReq.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAuth.App.Request
+{
+    public class BlockUpUserReq
+    {
+        public string UserId { get; set; }
+    }
+}
