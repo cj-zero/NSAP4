b74cc4c973ad2ccac2c108d45425ca7bddf6f0e1
刘俊繁 <liujunfan@neware.com.cn>
2020-08-21 10:30:03 +0800

    集成SignalR实时推送

---
+++ OpenAuth.Core/OpenAuth.App/SignalR/NameUserIdProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using OpenAuth.App.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAuth.App.SignalR
+{
+    public class NameUserIdProvider : IUserIdProvider
+    {
+        private readonly IAuth _authUtil;
+
+        public NameUserIdProvider(IAuth authUtil)
+        {
+            _authUtil = authUtil;
+        }
+
+        public string GetUserId(HubConnectionContext connection)
+        {
+            return  _authUtil.GetCurrentUser().User.Name;
+        }
+    }
+}
