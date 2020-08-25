6dddc7fec2109a9f7bfa4fee0f41323bf18f3081
刘俊繁 <liujunfan@neware.com.cn>
2020-08-25 15:24:35 +0800

    SignalR配置Redis

--- OpenAuth.Core/OpenAuth.App/SignalR/SignalRExtension.cs
+++ OpenAuth.Core/OpenAuth.App/SignalR/SignalRExtension.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,10 @@ namespace OpenAuth.App.SignalR
 {
     public static class SignalRExtension
     {
-        public static IServiceCollection AddNsapSignalR(this IServiceCollection services)
+        public static IServiceCollection AddNsapSignalR(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddSignalR().AddMessagePackProtocol();
+            var redis = configuration.GetSection("AppSetting:SignalR").GetValue<string>("Redis");
+            services.AddSignalR().AddMessagePackProtocol().AddRedis(redis);
             services.AddSingleton<IUserIdProvider, NameUserIdProvider>();
             return services;
         }
