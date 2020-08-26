6451293ff6bff272dd51b8de6ed6d39d6a854a6a
刘俊繁 <liujunfan@neware.com.cn>
2020-08-26 16:17:00 +0800

    二维码解析

---
+++ OpenAuth.Core/Infrastructure/QRCode/QRCodeReder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using ZXing;
+
+namespace Infrastructure.QRCode
+{
+    public class QRCodeReder
+    {
+        public static string Read(byte[] barcodeBitmap)
+        {
+            BarcodeReader reader = new BarcodeReader();
+            reader.Options.CharacterSet = "UTF-8";
+            var result = reader.Decode(barcodeBitmap);
+            return (result == null) ? null : result.Text;
+        }
+    }
+}
