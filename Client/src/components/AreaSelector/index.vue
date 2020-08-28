d27a0ca02b3fa6f47c50e52e71abc5318d33fd0c
hhm-lxb <lxbmisszsj@gmail.com>
2020-08-28 16:44:23 +0800

    布局样式调整

--- Client/src/components/AreaSelector/index.vue
+++ Client/src/components/AreaSelector/index.vue
@@ -5,196 +5,153 @@
     <ul class="tab-list">
       <li 
         v-for="(item, index) in tabList" 
-        :key="item.name" 
+        :key="item.areaName" 
         @click="selectTab(item, index)"
         :class="{ active: index === activeIndex }"
-      >{{ item.name }}</li>
+      >{{ item.areaName }}</li>
     </ul>
     <!-- 选择列表 -->
     <ul class="select-list">
       <li 
         v-for="item in selectList" 
-        :key="item.name" 
+        :key="item.areaName" 
         @click="selectItem(item)"
-        :class="{ active: includesName(item.name) }">{{ item.name }}</li>
+        :class="{ active: includesName(item.areaName) }">{{ item.areaName }}</li>
     </ul>
   </div>
 </template>
 
 <script>
-import addressList from './address'
+// import addressList from './address'
+import { getAreaList } from '@/api/serve/area'
 export default {
   components: {},
   data () {
     return {
       addressList: [],
-      tabList: [], // 所有选项卡的列表
+      tabList: [{
+        areaName: '请选择'
+      }], // 所有选项卡的列表
       selectList: [], // 当前选择的列表
       currentTab: '', // 当前选中的选项卡
       overseasList: [], // 海外国家列表
       activeIndex: 0, // 当前选中的选项卡
       currentIndex: 0,
       currentItem: {}, // 当前选择的数据
-      province: '', // 对应level1
-      city: '', // 对应level2
-      district: '', // 街道 对应level3
-      currentLevel: 1 // 默认level1
+      province: '', // 对应level2
+      city: '', // 对应level3
+      district: '', // 街道 对应level4
+      currentLevel: 1, // 默认level1
+      isFirst: true // 默认是第一次加载
     }
   },
   methods: {
-    _normalizeAddressList () {
-      addressList.forEach(item => {
-        let { name, childrens } = item
-        if (name === '亚洲') {
-          if (childrens.length) {
-            childrens.forEach(item => {
-              if (item.name === '中国') {
-                if (item.childrens.length) {
-                  this.selectList = this._normalizeChinaList(item)
-                }
-              } else {
-                this.overseasList.push({
-                  name: item.name,
-                  childrens: [],
-                  level: 2
-                }) // 亚洲其他地区精确到国家即可
-              }
-            })
-          }
-        } else { // 海外只精确到国家即可
-          if (childrens.length) {
-            childrens.forEach(item => {
-              this.overseasList.push({
-                name: item.name,
-                childrens: [],
-                level: 2
-              })
-            })
-          }
-        }
-      })
-    },
-    _normalizeChinaList (addressList, level = 1) {
-      let { childrens } = addressList
-      let result = []
-      childrens.forEach(item => {
-        let target = {
-          name: item.name,
-          level,
-          childrens: []
-        }
-        if(item.childrens.length) {
-          target.childrens = this._normalizeChinaList(item, level + 1)
-        }
-        result.push(target)
-      })
-      return result
-    },
     selectTab (item, index) {
-      let { childrens } = item
+      let { pid } = item
       console.log(item, 'item')
-      this.selectList = childrens
       this.activeIndex = index
       this.currentIndex = index
+      // if (index === 0) {
+      //   this._normalizeAddressList()
+      // } else {
+      //   // this.handleSelect(areaName, 'parent')
+      this._normalizeAddressList(pid)
+      // }
       console.log(item)
     },
     selectItem (item) { 
-      let { name } = item
-      this.tabList[this.currentIndex].name = name
-      this.handleSelect(item)
+      let { areaName, areaLevel } = item
+      this.currentItem = item
+      if (this.currentIndex === 0) {
+        this.tabList[0].pid = ''
+      }
+      this.tabList[this.currentIndex].areaName = areaName
+      this.handleSelect(areaName)
+      if (Number(areaLevel) === 3) {
+        this.$emit('change', {
+          province: this.province || '',
+          city: this.city || '',
+          district: this.district || ''
+        })
+        return this.closeSelector()
+      }
       console.log(item)
     },
-    handleSelect (item) {
-      let { level, name } = item
-      this.currentItem = item
-      console.log(this.currentItem, level, 'handleSelect')
-      switch (level) {
+    handleSelect (areaName) {
+      // console.log(this.currentItem, level, 'handleSelect')
+      switch (this.currentIndex) {
+        case 0:
+          this.province = areaName
+          break
         case 1:
-          this.province = name
+          this.city = areaName
           break
         case 2:
-          this.city = name
-          break
-        case 3:
-          this.district = name
+          this.district = areaName
           break
         default:
           break
       }
-      console.log(this.province)
     },
     includesName (name) {
       let { province, city, district } = this
       return [province, city, district].includes(name)
     },
-    closeSelector () {}
+    closeSelector () {
+      this.$emit("close")
+    },
+    _normalizeAddressList (id) {
+      getAreaList({
+        ReqId: id
+      }).then(res => {
+        this.selectList = res.data
+        console.log(this.selectList, 'selectList')
+        if (id && Number(this.currentItem.areaLevel) !== 3) {
+          this.tabList.push({
+            areaName: '请选择',
+            pid: id
+          })
+          this.activeIndex++
+          this.currentIndex++ 
+        }
+      })
+    }
   },
   watch: {
     province () {
       console.log('province')
-      let { childrens } = this.currentItem
-      if (this.tabList.length > 1) {
-        this.tabList = this.tabList.slice(0, 1)
-      }
-      if (childrens.length) {
-        this.tabList.push({
-          name: '请选择',
-          childrens
-        })
-        // this.tabList.splice(length - 1, 0, item) // 每次都往数组最后一项前面添加
-        this.activeIndex++
-        this.currentIndex++
-        this.selectList = childrens
+      let { id, areaLevel } = this.currentItem
+      if (Number(areaLevel) !== 3) {
+        if (this.tabList.length >= 2) {
+          this.tabList = this.tabList.slice(0, 1)
+        }
+        this._normalizeAddressList(id)
       }
     },
     city () {
-      let { childrens } = this.currentItem
-      if (this.tabList.length > 2) {
-        this.tabList = this.tabList.slice(0, 2)
-      }
-      if (childrens.length) {
-        this.tabList.push({
-          name: '请选择',
-          childrens
-        })
-        // this.tabList.splice(length - 1, 0, item) // 每次都往数组最后一项前面添加
-        this.activeIndex++
-        this.currentIndex++
-        this.selectList = childrens
+      let { id, areaLevel } = this.currentItem
+      if (Number(areaLevel) !== 3) { 
+        if (this.tabList.length >= 3) {
+          this.tabList = this.tabList.slice(0, 2)
+        }
+        this._normalizeAddressList(id)
       }
     },
     district () {
-      let { childrens } = this.currentItem
-      if (this.tabList.length > 3) {
-        this.tabList = this.tabList.slice(0, 3)
-      }
-      if (childrens.length) {
-        this.tabList.push({
-          name: '请选择',
-          childrens
-        })
-        // this.tabList.splice(length - 1, 0, item) // 每次都往数组最后一项前面添加
-        this.activeIndex++
-        this.currentIndex++
-        this.selectList = childrens
+      let { id, areaLevel } = this.currentItem
+      if (Number(areaLevel) !== 3) { 
+        if (this.tabList.length >= 4) {
+          this.tabList = this.tabList.slice(0, 4)
+        }
+        this._normalizeAddressList(id)
       }
     }
   },
   created () {
     this._normalizeAddressList()
-    this.selectList.push({ // 添加海外
-      name: '海外',
-      childrens: this.overseasList,
-      level: 1
-    })
-    this.tabList.push({
-      name: '请选择',
-      childrens: this.selectList,
-      level: 1
-    })
   },
   mounted () {
-
+    
   },
 }
 </script>
