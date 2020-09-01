54f78f7ef4d1b6cfd3bd76fcd3ad29585b07828c
superchen <neware.com.cn@DELL-E3490>
2020-09-01 18:49:16 +0800

    加服务统计报表接口

---
+++ OpenAuth.Core/OpenAuth.App/Serve/Response/ServiceOrderReportResp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAuth.App.Response
+{
+    public class ServerOrderStatListResp
+    {
+        /// <summary>
+        /// 统计类型
+        /// </summary>
+        public string StatType { get; set; }
+        /// <summary>
+        /// 统计数据
+        /// </summary>
+        public List<ServiceOrderReportResp> StatList { get; set; }
+    }
+    
+    public class ServiceOrderReportResp
+    {
+        /// <summary>
+        /// 统计键
+        /// </summary>
+        public string StatId
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 统计键值对应显示名称
+        /// </summary>
+        public string StatName { get; set; }
+        /// <summary>
+        /// 服务数量
+        /// </summary>
+        public int ServiceCnt { get; set; }
+    }
+}
