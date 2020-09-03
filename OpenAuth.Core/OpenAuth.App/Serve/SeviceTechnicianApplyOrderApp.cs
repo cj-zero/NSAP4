55c92a8640093c15e3822057e6c64228a4d726d9
yangt2020 <461192412@qq.com>
2020-09-03 11:17:32 +0800

    nsap核对设备

--- OpenAuth.Core/OpenAuth.App/Serve/SeviceTechnicianApplyOrderApp.cs
+++ OpenAuth.Core/OpenAuth.App/Serve/SeviceTechnicianApplyOrderApp.cs
@@ -197,7 +197,7 @@ namespace OpenAuth.App
             {
                 //派单给该技术员
                 await UnitWork.UpdateAsync<ServiceWorkOrder>(s => s.ServiceOrderId == request.ServiceOrderId && s.MaterialCode == request.MaterialCode, a => new ServiceWorkOrder { CurrentUserId = ApplyInfo.TechnicianId, Status = ApplyInfo.Status, OrderTakeType = (int)ApplyInfo.OrderTakeType, CurrentUserNsapId = ApplyInfo.CurrentUserNsapId, CurrentUser = ApplyInfo.CurrentUser });
-                await _serviceOrderLogApp.AddAsync(new AddOrUpdateServiceOrderLogReq { Action = $"系统派单给技术员{ApplyInfo.CurrentUser}派单", ActionType = "系统派单工单" });
+                await _serviceOrderLogApp.AddAsync(new AddOrUpdateServiceOrderLogReq { Action = $"系统派单给技术员{ApplyInfo.CurrentUser}派单", ActionType = "系统派单工单",ServiceOrderId=request.ServiceOrderId });
                 await _serviceOrderApp.PushMessageToApp((int)ApplyInfo.TechnicianId, "派单成功提醒", "您已被派有一个新的售后服务，请尽快处理");
             }
         }
@@ -257,7 +257,8 @@ namespace OpenAuth.App
                 s.a.Id,
                 s.a.FromTheme,
                 s.a.FromType,
-                s.a.ProblemTypeId
+                s.a.ProblemTypeId,
+                s.a.SolvedResult
             }).ToListAsync();
             result.Data = data;
             return result;
