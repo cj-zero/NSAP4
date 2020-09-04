27b598d9ca1d56840234208ae248be8c1cba406c
刘俊繁 <liujunfan@neware.com.cn>
2020-09-04 09:44:56 +0800

    无聊查询

---
+++ OpenAuth.Core/OpenAuth.App/Sap/Response/SerialNumberListResp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAuth.App.Response
+{
+    public class SerialNumberListResp
+    {
+        public string ManufSN { get; set; }
+        public string InternalSN { get; set; }
+        public string Customer { get; set; }
+        public string CustmrName { get; set; }
+        public int ContractID { get; set; }
+        public string ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public DateTime DlvryDate { get; set; }
+    }
+}
