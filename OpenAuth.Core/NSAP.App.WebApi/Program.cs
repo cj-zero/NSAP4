dd5bd935d8f67910f2d7c5ad6ff13ff5c6d52091
刘俊繁 <liujunfan@neware.com.cn>
2020-09-17 11:32:06 +0800

    修改日志配置

--- OpenAuth.Core/NSAP.App.WebApi/Program.cs
+++ OpenAuth.Core/NSAP.App.WebApi/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Autofac.Extensions.DependencyInjection;
+using Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -16,29 +17,24 @@ namespace NSAP.App.WebApi
     {
         public static void Main(string[] args)
         {
-            // 配置 Serilog 
-            Log.Logger = new LoggerConfiguration()
-                // 最小的日志输出级别
-                .MinimumLevel.Information()
-                // 日志调用类命名空间如果以 Microsoft 开头，覆盖日志输出最小级别为 Information
-                //.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                .Enrich.FromLogContext()
-                // 配置日志输出到控制台
-                .WriteTo.Console()
-                // 配置日志输出到文件，文件输出到当前项目的 logs 目录下
-                // 日记的生成周期为每天
-                .WriteTo.File(Path.Combine("logs", @"log.txt"), rollingInterval: RollingInterval.Day)
-                // 创建 logger
-                .CreateLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.ClearProviders(); //去掉默认的日志
+                    logging.AddFilter("System", LogLevel.Error);
+                    logging.AddFilter("Microsoft", LogLevel.Error);
+                    logging.AddSerilog();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>()
-                    .UseSerilog();
+                    webBuilder
+                    .UseSerilog((context, configuration) =>
+                             configuration.BuildSerilogLogger(context.Configuration)
+                        ).UseStartup<Startup>();
                 })
                 .UseServiceProviderFactory(
                     new AutofacServiceProviderFactory());
