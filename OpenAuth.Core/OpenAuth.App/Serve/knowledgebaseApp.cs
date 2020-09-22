1cea5f80fea86421a6d23afc9bfedecc6c6f56f3
刘俊繁 <liujunfan@neware.com.cn>
2020-09-22 09:34:29 +0800

    知识库

---
+++ OpenAuth.Core/OpenAuth.App/Serve/knowledgebaseApp.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using OpenAuth.App.Interface;
+using OpenAuth.App.Request;
+using OpenAuth.App.Response;
+using OpenAuth.Repository.Domain;
+using OpenAuth.Repository.Interface;
+
+
+namespace OpenAuth.App
+{
+    public class KnowledgeBaseApp : BaseTreeApp<KnowledgeBase>
+    {
+        private RevelanceManagerApp _revelanceApp;
+
+        /// <summary>
+        /// 加载列表
+        /// </summary>
+        public async Task<TableData> Load(QueryKnowledgeBaseListReq request)
+        {
+            var loginContext = _auth.GetCurrentUser();
+            if (loginContext == null)
+            {
+                throw new CommonException("登录已过期", Define.INVALID_TOKEN);
+            }
+
+
+            var result = new TableData();
+            var objs = UnitWork.Find<KnowledgeBase>(null);
+            if (!string.IsNullOrEmpty(request.key))
+            {
+                objs = objs.Where(u => u.Id.Contains(request.key));
+            }
+
+            result.Data = await objs.OrderBy(u => u.Id)
+                .Skip((request.page - 1) * request.limit)
+                .Take(request.limit).ToListAsync();
+            result.Count = objs.Count();
+            return result;
+        }
+
+        public void Add(AddOrUpdateKnowledgeBaseReq req)
+        {
+            var obj = req.MapTo<KnowledgeBase>();
+            //todo:补充或调整自己需要的字段
+            obj.CreateTime = DateTime.Now;
+            var user = _auth.GetCurrentUser().User;
+            obj.CreateUserId = user.Id;
+            obj.CreateUserName = user.Name;
+            Repository.Add(obj);
+        }
+
+         public void Update(AddOrUpdateKnowledgeBaseReq obj)
+        {
+            var user = _auth.GetCurrentUser().User;
+            UnitWork.Update<KnowledgeBase>(u => u.Id == obj.Id, u => new KnowledgeBase
+            {
+                SequenceNumber = obj.SequenceNumber,
+                Type = obj.Type,
+                Org = obj.Org,
+                Name = obj.Name,
+                Content = obj.Content,
+                ParentId = obj.ParentId,
+                ParentName = obj.ParentName,
+                CascadeId = obj.CascadeId,
+                UpdateTime = DateTime.Now,
+                UpdateUserId = user.Id,
+                UpdateUserName = user.Name
+                //todo:补充或调整自己需要的字段
+            });
+
+        }
+            
+
+        public KnowledgeBaseApp(IUnitWork unitWork, IRepository<KnowledgeBase> repository,
+            RevelanceManagerApp app, IAuth auth) : base(unitWork, repository,auth)
+        {
+            _revelanceApp = app;
+        }
+    }
+}
---
+++ OpenAuth.Core/OpenAuth.WebApi/Controllers/Serve/KnowledgeBasesController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Threading.Tasks;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using OpenAuth.App;
+using OpenAuth.App.Request;
+using OpenAuth.App.Response;
+using OpenAuth.Repository.Domain;
+
+namespace OpenAuth.WebApi.Controllers
+{
+    /// <summary>
+    /// knowledgebase操作
+    /// </summary>
+    [Route("api/Serve/[controller]/[action]")]
+    [ApiController]
+    public class KnowledgeBasesController : ControllerBase
+    {
+        private readonly KnowledgeBaseApp _app;
+        
+        //获取详情
+        [HttpGet]
+        public Response<KnowledgeBase> Get(string id)
+        {
+            var result = new Response<KnowledgeBase>();
+            try
+            {
+                result.Result = _app.Get(id);
+            }
+            catch (Exception ex)
+            {
+                result.Code = 500;
+                result.Message = ex.InnerException?.Message ?? ex.Message;
+            }
+
+            return result;
+        }
+
+        //添加
+       [HttpPost]
+        public Response Add(AddOrUpdateKnowledgeBaseReq obj)
+        {
+            var result = new Response();
+            try
+            {
+                _app.Add(obj);
+
+            }
+            catch (Exception ex)
+            {
+                result.Code = 500;
+                result.Message = ex.InnerException?.Message ?? ex.Message;
+            }
+
+            return result;
+        }
+
+        //修改
+       [HttpPost]
+        public Response Update(AddOrUpdateKnowledgeBaseReq obj)
+        {
+            var result = new Response();
+            try
+            {
+                _app.Update(obj);
+
+            }
+            catch (Exception ex)
+            {
+                result.Code = 500;
+                result.Message = ex.InnerException?.Message ?? ex.Message;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 加载列表
+        /// </summary>
+        [HttpGet]
+        public async Task<TableData> Load([FromQuery] QueryKnowledgeBaseListReq request)
+        {
+            return await _app.Load(request);
+        }
+
+        /// <summary>
+        /// 批量删除
+        /// </summary>
+       [HttpPost]
+        public Response Delete([FromBody]string[] ids)
+        {
+            var result = new Response();
+            try
+            {
+                _app.Delete(ids);
+
+            }
+            catch (Exception ex)
+            {
+                result.Code = 500;
+                result.Message = ex.InnerException?.Message ?? ex.Message;
+            }
+
+            return result;
+        }
+
+        public KnowledgeBasesController(KnowledgeBaseApp app) 
+        {
+            _app = app;
+        }
+    }
+}
