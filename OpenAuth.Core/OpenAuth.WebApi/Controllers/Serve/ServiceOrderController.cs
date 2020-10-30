9332b9be67705db9ba6fbd87136da61a3a7006d8
张立国 <ITmoxiang@163.com>
2020-10-30 16:02:06 +0800

    修改工单服务单数量推送和新增消息推送

--- OpenAuth.Core/OpenAuth.WebApi/Controllers/Serve/ServiceOrderController.cs
+++ OpenAuth.Core/OpenAuth.WebApi/Controllers/Serve/ServiceOrderController.cs
@@ -591,7 +591,29 @@ namespace OpenAuth.WebApi.Controllers
             }
             return result;
         }
-        
+
+        /// <summary>
+        /// 消息已读
+        /// </summary>
+        /// <param name="req"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<Response> ReadMsg(ReadMsgReq req)
+        {
+            var result = new Response();
+            try
+            {
+                await _serviceOrderApp.ReadMsg(req.currentUserId, req.serviceOrderId);
+            }
+            catch (Exception ex)
+            {
+                result.Code = 500;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+
+
         #endregion
 
         #region App售后接口 如无特殊情况勿动，修改请告知！！！
@@ -726,6 +748,7 @@ namespace OpenAuth.WebApi.Controllers
             return result;
         }
 
+        
         #endregion
 
         #region<<Customer>>
