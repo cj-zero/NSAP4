0b9f1c68467f7ad33a1a8f43a29d2cc98cb6a576
刘俊繁 <liujunfan@neware.com.cn>
2020-11-05 14:26:25 +0800

    查询物流例子

--- OpenAuth.Core/Infrastructure/KuaiDi100/Core/QueryTrack.cs
+++ OpenAuth.Core/Infrastructure/KuaiDi100/Core/QueryTrack.cs
@@ -6,16 +6,28 @@ using Utils;
 using Common;
 using Common.Request;
 
-public class QueryTrack{
+public class QueryTrack
+{
 
+    /// <summary>
+    /// QueryTrack.query(new QueryTrackReq()
+    /// {
+    ///           customer = config.customer,
+    ///           sign = SignUtils.GetMD5(queryTrackParam.ToString() + config.key + config.customer),
+    ///           param =  queryTrackParam
+    /// });
+    /// </summary>
+    /// <param name="query"></param>
+    /// <returns></returns>
+    public static string query(QueryTrackReq query)
+    {
 
-    public static string query(QueryTrackReq query){
-        
         var request = ObjectToDictionaryUtils.ObjectToMap(query);
-        if(request == null){
+        if (request == null)
+        {
             return null;
         }
-        var result = HttpUtils.doPostForm(ApiInfoConstant.QUERY_URL,request);
+        var result = HttpUtils.doPostForm(ApiInfoConstant.QUERY_URL, request);
         return result;
     }
 }   
