fda7bbbb8616332510047a8c0ab4272a2de18910
刘俊繁 <liujunfan@neware.com.cn>
2020-12-04 10:55:24 +0800

    BTS API init

---
+++ OpenAuth.Core/OpenAuth.App/Nwcali/BtsGroupApp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Infrastructure;
+using OpenAuth.App.Interface;
+using OpenAuth.App.Request;
+using OpenAuth.App.Response;
+using OpenAuth.Repository.Domain;
+using OpenAuth.Repository.Interface;
+
+
+namespace OpenAuth.App
+{
+    public class BtsGroupApp : OnlyUnitWorkBaeApp
+    {
+        private RevelanceManagerApp _revelanceApp;
+
+        /// <summary>
+        /// 加载列表
+        /// </summary>
+        public TableData Load(QuerybtsgroupListReq request)
+        {
+            var loginContext = _auth.GetCurrentUser();
+            if (loginContext == null)
+            {
+                throw new CommonException("登录已过期", Define.INVALID_TOKEN);
+            }
+
+
+
+            var result = new TableData();
+            var objs = UnitWork.Find<BtsGroup>(null);
+            if (!string.IsNullOrEmpty(request.key))
+            {
+                objs = objs.Where(u => u.Group.Contains(request.key));
+            }
+
+
+            result.Data = objs.OrderBy(u => u.Id)
+                .Skip((request.page - 1) * request.limit)
+                .Take(request.limit).ToList();
+            result.Count = objs.Count();
+            return result;
+        }
+
+        public void Add(AddOrUpdatebtsgroupReq req)
+        {
+            var obj = req.MapTo<BtsGroup>();
+            //todo:补充或调整自己需要的字段
+            var user = _auth.GetCurrentUser().User;
+            UnitWork.Add<BtsGroup,int>(obj);
+        }
+
+         public void Update(AddOrUpdatebtsgroupReq obj)
+        {
+            var user = _auth.GetCurrentUser().User;
+            UnitWork.Update<BtsGroup>(u => u.Id == obj.Id, u => new BtsGroup
+            {
+                Group = obj.Group,
+                //todo:补充或调整自己需要的字段
+            });
+
+        }
+            
+
+        public BtsGroupApp(IUnitWork unitWork,
+            RevelanceManagerApp app, IAuth auth) : base(unitWork, auth)
+        {
+            _revelanceApp = app;
+        }
+
+        public void Delete(List<int> ids)
+        {
+            UnitWork.Delete<BtsGroup>(g => ids.Contains(g.Id));
+        }
+    }
+}
---
+++ OpenAuth.Core/OpenAuth.WebApi/Controllers/Nwcali/BtsGroupController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OpenAuth.App;
+using OpenAuth.App.Request;
+using OpenAuth.App.Response;
+using OpenAuth.Repository.Domain;
+
+namespace OpenAuth.WebApi.Controllers
+{
+    /// <summary>
+    /// btsgroup操作
+    /// </summary>
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BtsGroupController : ControllerBase
+    {
+        private readonly BtsGroupApp _app;
+
+
+        //添加
+        [HttpPost]
+        public Response Add(AddOrUpdatebtsgroupReq obj)
+        {
+            var result = new Response();
+            _app.Add(obj);
+
+
+            return result;
+        }
+
+        //修改
+        [HttpPost]
+        public Response Update(AddOrUpdatebtsgroupReq obj)
+        {
+            var result = new Response();
+            _app.Update(obj);
+
+            return result;
+        }
+
+        /// <summary>
+        /// 加载列表
+        /// </summary>
+        [HttpGet]
+        public TableData Load([FromQuery] QuerybtsgroupListReq request)
+        {
+            return _app.Load(request);
+        }
+
+        /// <summary>
+        /// 批量删除
+        /// </summary>
+        [HttpPost]
+        public Response Delete([FromBody] List<int> ids)
+        {
+            var result = new Response();
+            _app.Delete(ids);
+
+            return result;
+        }
+
+        public BtsGroupController(BtsGroupApp app)
+        {
+            _app = app;
+        }
+    }
+}
