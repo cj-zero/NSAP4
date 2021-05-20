19cbcfc0e97e88baa2f9f7ec87a5db4b94f20f22
张立国 <ITmoxiang@163.com>
2021-05-20 13:48:40 +0800

    feat:新增应收贷项凭证

---
+++ OpenAuth.Core/Sap.Handler/Sap/Define.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sap.Handler.Sap
+{
+    public class Define
+    {
+        public const int SBO_ID = 1;//ERP3.0当前账套
+    }
+}
