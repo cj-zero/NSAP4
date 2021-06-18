c39b104dab2a01d5000541b9f776a726ff8da26d
张立国 <ITmoxiang@163.com>
2021-06-18 15:53:56 +0800

    fix:业务伙伴查询

---
+++ OpenAuth.Core/OpenAuth.Repository/Domain/NsapUserMap.cs
@@ -0,0 +1,33 @@
+﻿using OpenAuth.Repository.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace OpenAuth.Repository.Domain
+{
+    /// <summary>
+    /// 与erp3.0用户绑定表
+    /// </summary>
+    [Table("NsapUserMap")]
+    public class NsapUserMap : Entity
+    {
+        public NsapUserMap()
+        {
+            this.UserID = string.Empty;
+        }
+
+        /// <summary>
+        /// NSAP的UserId
+        /// </summary>
+        [Description("NSAP的UserId")]
+        public string UserID { get; set; }
+        /// <summary>
+        /// ERP3.0的UserId
+        /// </summary>
+        [Description("ERP3.0的UserId")]
+        [Browsable(false)]
+        public int? NsapUserId { get; set; }
+    }
+}
