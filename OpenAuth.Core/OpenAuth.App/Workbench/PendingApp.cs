6f3c1907b34958847e2fd15889745115bbb87752
张立国 <ITmoxiang@163.com>
2021-07-07 10:29:14 +0800

    fix:待处理查看列表

--- OpenAuth.Core/OpenAuth.App/Workbench/PendingApp.cs
+++ OpenAuth.Core/OpenAuth.App/Workbench/PendingApp.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Infrastructure;
+using Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
 using OpenAuth.App.Interface;
 using OpenAuth.App.Response;
+using OpenAuth.App.Workbench.Request;
 using OpenAuth.App.Workbench.Response;
 using OpenAuth.Repository.Domain;
 using OpenAuth.Repository.Domain.Workbench;
@@ -30,7 +33,7 @@ namespace OpenAuth.App.Workbench
         /// 服务单详情
         /// </summary>
         /// <returns></returns>
-        public async Task<ServiceOrderResp> ServiceOrderDetails(string ServiceOrderId) 
+        public async Task<ServiceOrderResp> ServiceOrderDetails(string ServiceOrderId)
         {
 
             return new ServiceOrderResp();
@@ -96,7 +99,7 @@ namespace OpenAuth.App.Workbench
                     TerminalCustomer = obj.TerminalCustomer,
                     TerminalCustomerId = obj.TerminalCustomerId,
                     ServiceOrderId = obj.ServiceOrderId,
-                    ServiceOrderSapId =obj.ServiceOrderSapId,
+                    ServiceOrderSapId = obj.ServiceOrderSapId,
                     UpdateTime = obj.UpdateTime,
                     Remark = obj.Remark,
                     FlowInstanceId = obj.FlowInstanceId,
@@ -112,11 +115,38 @@ namespace OpenAuth.App.Workbench
         ///判断增加还是修改待处理
         /// </summary>
         /// <returns></returns>
-        public async Task<TableData> Load()
+        public async Task<TableData> Load(PendingReq req)
         {
+            var loginContext = _auth.GetCurrentUser();
+            if (loginContext == null)
+            {
+                throw new CommonException("登录已过期", Define.INVALID_TOKEN);
+            }
             var reult = new TableData();
-            //增加全局待处理
-            reult.Data = await UnitWork.Find<WorkbenchPending>(null).ToListAsync();
+            var query = from a in UnitWork.Find<WorkbenchPending>(null)
+                        join b in UnitWork.Find<FlowInstance>(null) on a.FlowInstanceId equals b.Id
+                        where b.MakerList.Contains(loginContext.User.Id)
+                        select new { a, b };
+            query = query.WhereIf(!string.IsNullOrWhiteSpace(req.ApprovalNumber), q => q.a.ApprovalNumber == int.Parse(req.ApprovalNumber))
+                        .WhereIf(!string.IsNullOrWhiteSpace(req.Petitioner), q => q.a.Petitioner.Contains(req.Petitioner))
+                        .WhereIf(!string.IsNullOrWhiteSpace(req.TerminalCustomer), q => q.a.TerminalCustomer.Contains(req.TerminalCustomer))
+                        .WhereIf(!string.IsNullOrWhiteSpace(req.TerminalCustomerId), q => q.a.TerminalCustomerId.Contains(req.TerminalCustomerId))
+                        .WhereIf(!string.IsNullOrWhiteSpace(req.StartTime.ToString()), q => q.a.UpdateTime > req.StartTime)
+                        .WhereIf(!string.IsNullOrWhiteSpace(req.EndTime.ToString()), q => q.a.UpdateTime > Convert.ToDateTime(req.EndTime).AddDays(1));
+            var pending = await query.Select(q => new {
+                q.a.ApprovalNumber,
+                q.a.TerminalCustomer,
+                q.a.TerminalCustomerId,
+                q.a.SourceNumbers,
+                q.a.ServiceOrderSapId,
+                q.a.Remark,
+                q.a.Petitioner,
+                q.a.OrderType,
+                q.b.ActivityName,
+                q.a.UpdateTime
+            }).Skip((req.page - 1) * req.limit).Take(req.limit).ToListAsync();
+            reult.Data = pending;
+            reult.Count = await query.CountAsync();
             return reult;
         }
 
--- OpenAuth.Core/OpenAuth.App/Workbench/Request/PendingReq.cs
+++ OpenAuth.Core/OpenAuth.App/Workbench/Request/PendingReq.cs
@@ -1,14 +1,40 @@
-﻿using System;
+﻿using OpenAuth.App.Request;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace OpenAuth.App.Workbench.Request
 {
-    public class PendingReq
+    public class PendingReq: PageReq
     {
         /// <summary>
-        /// 审批Id
+        /// 审批序号
         /// </summary>
-        public string ApprovalId { get; set; }
+        public string ApprovalNumber { get; set; }
+
+        /// <summary>
+        /// 客户名称
+        /// </summary>
+        public string TerminalCustomer { get; set; }
+
+        /// <summary>
+        /// 客户代码
+        /// </summary>
+        public string TerminalCustomerId { get; set; }
+
+        /// <summary>
+        /// 申请人
+        /// </summary>
+        public string Petitioner { get; set; }
+
+        /// <summary>
+        /// 更新时间开始
+        /// </summary>
+        public DateTime? StartTime { get; set; }
+
+        /// <summary>
+        /// 更新时间结束
+        /// </summary>
+        public DateTime? EndTime { get; set; }
     }
 }
