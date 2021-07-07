d5d42e6923daaf45cbdcbae6f17df86eea2bdf3e
张立国 <ITmoxiang@163.com>
2021-07-07 13:47:26 +0800

    fix:修改待处理查看接口

--- OpenAuth.Core/OpenAuth.WebApi/Controllers/Workbench/PendingController.cs
+++ OpenAuth.Core/OpenAuth.WebApi/Controllers/Workbench/PendingController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using OpenAuth.App.Response;
 using OpenAuth.App.Workbench;
+using OpenAuth.App.Workbench.Request;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +10,16 @@ using System.Threading.Tasks;
 
 namespace OpenAuth.WebApi.Controllers.Workbench
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     [ApiExplorerSettings(GroupName = "Workbench")]
     public class PendingController : ControllerBase
     {
         private readonly PendingApp _app;
         [HttpGet]
-        public async Task<TableData> Load() 
+        public async Task<TableData> Load(PendingReq req) 
         {
-            return await _app.Load();
+            return await _app.Load(req);
         }
        
         public PendingController(PendingApp app)
