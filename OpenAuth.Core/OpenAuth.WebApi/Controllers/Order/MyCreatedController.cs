bccbdcc585a5f75d805f6568e8f7b9055b1ff1fd
李节锋 <1902443457@qq.com>
2021-08-27 17:21:02 +0800

    控制器更名

--- OpenAuth.Core/OpenAuth.WebApi/Controllers/Order/MyCreatedController.cs
+++ OpenAuth.Core/OpenAuth.WebApi/Controllers/Order/MyCreatedController.cs
@@ -19,13 +19,13 @@ namespace OpenAuth.WebApi.Controllers.Order
 	[Route("api/Order/[controller]")]
 	[ApiController]
 	[ApiExplorerSettings(GroupName = "Order")]
-	public class ICreatedController : Controller
+	public class MyCreatedController : Controller
 	{
 		private readonly ServiceSaleOrderApp _serviceSaleOrderApp;
 		IAuth _auth;
 		IUnitWork UnitWork;
 		ServiceBaseApp _serviceBaseApp;
-		public ICreatedController(IUnitWork UnitWork, ServiceBaseApp _serviceBaseApp, IAuth _auth, ServiceSaleOrderApp serviceSaleOrderApp)
+		public MyCreatedController(IUnitWork UnitWork, ServiceBaseApp _serviceBaseApp, IAuth _auth, ServiceSaleOrderApp serviceSaleOrderApp)
 		{
 			this.UnitWork = UnitWork;
 			this._serviceBaseApp = _serviceBaseApp;
@@ -43,7 +43,8 @@ namespace OpenAuth.WebApi.Controllers.Order
 			var UserID = _serviceBaseApp.GetUserNaspId();
 			var result = new TableData();
 
-			DataTable dt = _serviceSaleOrderApp.GetICreated(model.limit, model.page, model.query, model.sortname, model.sortorder, UserID, model.types, model.Applicator, model.Customer, model.Status, model.BeginDate, model.EndDate, _serviceSaleOrderApp.GetPagePowersByUrl("mywork/AuditAllNew.aspx",UserID).ViewCustom, _serviceSaleOrderApp.GetPagePowersByUrl("mywork/AuditAllNew.aspx",UserID).ViewSales);
+			//DataTable dt = _serviceSaleOrderApp.GetICreated(model.limit, model.page, model.query, model.sortname, model.sortorder, UserID, model.types, model.Applicator, model.Customer, model.Status, model.BeginDate, model.EndDate, _serviceSaleOrderApp.GetPagePowersByUrl("mywork/AuditAllNew.aspx",UserID).ViewCustom, _serviceSaleOrderApp.GetPagePowersByUrl("mywork/AuditAllNew.aspx",UserID).ViewSales);
+			DataTable dt = _serviceSaleOrderApp.GetICreated(model.limit, model.page, model.query, model.sortname, model.sortorder, UserID, model.types, model.Applicator, model.Customer, model.Status, model.BeginDate, model.EndDate,true,true);
 
 			result.Data = dt;
 			result.Count = dt.Rows.Count;
