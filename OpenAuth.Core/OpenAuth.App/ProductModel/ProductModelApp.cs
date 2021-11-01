ad39d9f3476749d7d38faa05c269bd3df3bde010
Ljyi <979671716@qq.com>
2021-11-01 14:26:16 +0800

    产品选型

---
+++ OpenAuth.Core/OpenAuth.App/ProductModel/ProductModelApp.cs
@@ -0,0 +1,148 @@
+﻿using Infrastructure;
+using Newtonsoft.Json;
+using OpenAuth.App.Interface;
+using OpenAuth.App.ProductModel;
+using OpenAuth.App.ProductModel.Request;
+using OpenAuth.App.Response;
+using OpenAuth.Repository.Domain;
+using OpenAuth.Repository.Domain.ProductModel;
+using OpenAuth.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAuth.App
+{
+    /// <summary>
+    /// 商品选项服务
+    /// </summary>
+    public class ProductModelApp : OnlyUnitWorkBaeApp
+    {
+        private RevelanceManagerApp _revelanceApp;
+        public ProductModelApp(IUnitWork unitWork, IAuth auth) : base(unitWork, auth)
+        {
+        }
+        /// <summary>
+        /// 获取设备编码
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetDeviceCodingList()
+        {
+            var productModelSelections = UnitWork.Find<ProductModelSelection>(u => !u.IsDelete);
+            return productModelSelections.Select(zw => zw.DeviceCoding).OrderBy(zw => zw).Distinct().ToList();
+        }
+        /// <summary>
+        /// 获取产品系列
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetProductTypeList()
+        {
+            return new List<string>();
+        }
+        /// <summary>
+        /// 获取电流等级
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetVoltageList()
+        {
+            var productModelSelections = UnitWork.Find<ProductModelSelection>(u => !u.IsDelete);
+            return productModelSelections.Select(zw => zw.Voltage).Distinct().OrderBy(zw => zw).ToList();
+        }
+        /// <summary>
+        /// 获取电压等级
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetCurrentList()
+        {
+            var productModelSelections = UnitWork.Find<ProductModelSelection>(u => !u.IsDelete);
+            return productModelSelections.Select(zw => zw.Current).Distinct().OrderBy(zw => zw).ToList();
+        }
+        /// <summary>
+        /// 获取通道数量
+        /// </summary>
+        /// <returns></returns>
+        public List<int> GetChannelList()
+        {
+            var productModelSelections = UnitWork.Find<ProductModelSelection>(u => !u.IsDelete);
+            return productModelSelections.Select(zw => zw.ChannelNumber).Distinct().OrderBy(zw => zw).ToList();
+        }
+        /// <summary>
+        /// 获取功率
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetTotalPowerList()
+        {
+            var productModelSelections = UnitWork.Find<ProductModelSelection>(u => !u.IsDelete);
+            return productModelSelections.Select(zw => zw.TotalPower).Distinct().OrderBy(zw => zw).ToList();
+        }
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <returns></returns>
+        public List<ProductModelInfo> GetProductModelGrid(ProductModelReq queryModel, out int rowcount)
+        {
+            Expression<Func<ProductModelSelection, bool>> exps = t => true;
+            exps = exps.And(t => !t.IsDelete);
+            if (!string.IsNullOrWhiteSpace(queryModel.ProductType))
+            {
+                exps = exps.And(t => t.ProductType == queryModel.ProductType);
+            }
+            if (!string.IsNullOrWhiteSpace(queryModel.DeviceCoding))
+            {
+                exps = exps.And(t => t.ProductType == queryModel.DeviceCoding);
+            }
+            if (!string.IsNullOrWhiteSpace(queryModel.Voltage))
+            {
+                exps = exps.And(t => t.Voltage == queryModel.Voltage);
+            }
+            if (!string.IsNullOrWhiteSpace(queryModel.Current))
+            {
+                exps = exps.And(t => t.Current == queryModel.Current);
+            }
+            if (!string.IsNullOrWhiteSpace(queryModel.TotalPower))
+            {
+                exps = exps.And(t => t.TotalPower == queryModel.TotalPower);
+            }
+            if (queryModel.ChannelNumber > 0)
+            {
+                exps = exps.And(t => t.ChannelNumber == queryModel.ChannelNumber);
+            }
+            var productModelSelectionList = UnitWork.Find(queryModel.page, queryModel.limit, "", exps);
+            rowcount = UnitWork.GetCount(exps);
+            return productModelSelectionList.MapToList<ProductModelInfo>();
+        }
+        /// <summary>
+        /// 获取产品手册
+        /// </summary>
+        /// <param name="ProductModelCategoryId"></param>
+        /// <returns></returns>
+        public List<string> GetProductImg(int ProductModelCategoryId)
+        {
+            List<string> imgs = new List<string>();
+            var productModelCategory = UnitWork.Find<ProductModelCategory>(u => !u.IsDelete && u.Id == ProductModelCategoryId)?.FirstOrDefault();
+            if (productModelCategory != null && !string.IsNullOrWhiteSpace(productModelCategory.Image))
+            {
+                imgs = JsonConvert.DeserializeObject<List<string>>(productModelCategory.Image);
+            }
+            return imgs;
+        }
+        /// <summary>
+        /// 获取产品手册
+        /// </summary>
+        /// <param name="ProductModelCategoryId"></param>
+        /// <returns></returns>
+        public List<string> GetCaseImage(int ProductModelCategoryId)
+        {
+            List<string> imgs = new List<string>();
+            var productModelCategory = UnitWork.Find<ProductModelCategory>(u => !u.IsDelete && u.Id == ProductModelCategoryId)?.FirstOrDefault();
+            if (productModelCategory != null && !string.IsNullOrWhiteSpace(productModelCategory.CaseImage))
+            {
+                imgs = JsonConvert.DeserializeObject<List<string>>(productModelCategory.CaseImage);
+            }
+            return imgs;
+        }
+    }
+}
---
+++ OpenAuth.Core/OpenAuth.App/ProductModel/ProductModelInfo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAuth.App.ProductModel
+{
+    public class ProductModelInfo
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// 分类Id
+        /// </summary>
+        public int ProductModelCategoryId { get; set; }
+        /// <summary>
+        /// 序列号
+        /// </summary>
+        public string SerialNumber { get; set; }
+        /// <summary>
+        /// 产品类型
+        /// </summary>
+        public string ProductType { get; set; }
+        /// <summary>
+        /// 设备编码
+        /// </summary>
+        public string DeviceCoding { get; set; }
+        /// <summary>
+        /// 电压
+        /// </summary>
+        public string Voltage { get; set; }
+        /// <summary>
+        /// 电流
+        /// </summary>
+        public string Current { get; set; }
+        /// <summary>
+        /// 序列号
+        /// </summary>
+        public int ChannelNumber { get; set; }
+        /// <summary>
+        /// 功率
+        /// </summary>
+        public string TotalPower { get; set; }
+        /// <summary>
+        /// 电流精度
+        /// </summary>
+        public string CurrentAccurack { get; set; }
+        /// <summary>
+        /// 设备尺寸
+        /// </summary>
+        public string Size { get; set; }
+        /// <summary>
+        /// 重量
+        /// </summary>
+        public double Weight { get; set; }
+        /// <summary>
+        /// 价格
+        /// </summary>
+        public decimal UnitPrice { get; set; }
+    }
+}
