1341ca59bf555f3ee7955a6c2a5b7bcb5e56e203
Ljyi <979671716@qq.com>
2021-11-16 15:44:48 +0800

    销售报价单同步

---
+++ OpenAuth.Core/Sap.Handler/Service/ServiceSaleOrderHandler.cs
@@ -0,0 +1,38 @@
+﻿using DotNetCore.CAP;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using OpenAuth.Repository;
+using OpenAuth.Repository.Domain;
+using OpenAuth.Repository.Interface;
+using SAPbobsCOM;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenAuth.Repository.Domain.Sap;
+using System.Reactive;
+using Sap.Handler.Service.Request;
+
+namespace Sap.Handler.Service
+{
+    /// <summary>
+    /// 销售报价单同步SAP
+    /// </summary>
+    public class ServiceSaleOrderHandler : ICapSubscribe
+    {
+        private readonly IUnitWork UnitWork;
+        private readonly Company company;
+
+        public ServiceSaleOrderHandler(IUnitWork unitWork, Company company)
+        {
+            UnitWork = unitWork;
+            this.company = company;
+        }
+        [CapSubscribe("Serve.ServiceSaleOrder.Create")]
+        public async Task HandleServiceOrder(int theServiceOrderId)
+        {
+        }
+    }
+}
