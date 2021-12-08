252b76ceca3f2cd8b0bd887001affda962fea33a
李节锋 <1902443457@qq.com>
2021-12-08 17:57:36 +0800

    登录token过期处理，新增销售交货详情接口（60%）

--- OpenAuth.Core/OpenAuth.App/Order/SalesDeliveryApp.cs
+++ OpenAuth.Core/OpenAuth.App/Order/SalesDeliveryApp.cs
@@ -1,40 +1,14 @@
 ﻿extern alias MySqlConnectorAlias;
-using DotNetCore.CAP;
-using Infrastructure;
 using Infrastructure.Extensions;
-using Microsoft.Extensions.Options;
-using MySql.Data.MySqlClient;
 using OpenAuth.App.Interface;
 using OpenAuth.App.Order.Request;
-using OpenAuth.Repository.Domain;
-using OpenAuth.Repository.Domain.NsapBase;
-using Microsoft.Data.SqlClient;
-using OpenAuth.App.Request;
-using OpenAuth.App.Response;
 using OpenAuth.Repository;
-using OpenAuth.Repository.Extensions;
 using OpenAuth.Repository.Interface;
-using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Text;
-using OpenAuth.App.Order.ModelDto;
-using OpenAuth.Repository.Domain.Sap;
 using NSAP.Entity.Sales;
-using Infrastructure.Helpers;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-using static Infrastructure.Helpers.CommonHelper;
-using NSAP.Entity.BillFlow;
-using NSAP.Entity.Store;
-using NSAP.Entity.Product;
-using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using System.Net.Http.Headers;
-using Infrastructure.Export;
 
 namespace OpenAuth.App.Order
 {
@@ -51,13 +25,15 @@ namespace OpenAuth.App.Order
             this._serviceSaleOrderApp = serviceSaleOrderApp;
 
         }
-        public async Task<string> SalesDeliverySave(SalesOrderSaveReq salesOrderSaveReq, int UserID, int FuncID, string jobdata,
+        public async Task<string> SalesDeliverySave(SalesDeliverySaveReq salesDeliverySaveReq, int UserID, int FuncID, string jobdata,
             string jobname, int SboID, string IsTemplate)
         {
             string result = "", className = "";
             if (jobname == "销售交货") { className = "NSAP.B1Api.BOneODLN"; }
-            billDelivery Model = _serviceSaleOrderApp.GetDeliverySalesInfoNewNos(salesOrderSaveReq.JobId.ToString(), UserID);
+            billDelivery Model = _serviceSaleOrderApp.GetDeliverySalesInfoNewNos(salesDeliverySaveReq.JobId.ToString(), UserID);
             Model.DocStatus = "O";
+            Model.Comments += "基于销售订单" + salesDeliverySaveReq.JobId;
+            Model.CustomFields = salesDeliverySaveReq.CustomFields;
             #region 必须都有关联订单，并且购买数量与关联订单数量一致,采购订单所有物料高于2次的采购历史，并且价格不高于历史最低价，则不需审批直接通过。
             bool PurPassAudit = false;
             #endregion
@@ -66,13 +42,13 @@ namespace OpenAuth.App.Order
             #endregion
             //审批流单据金额
             double jobDocTotal = (double.Parse(Model.DocTotal) > 0 ? double.Parse(Model.DocTotal) : 0);
-            if (!string.IsNullOrEmpty(salesOrderSaveReq.Ations.ToString()))
+            if (!string.IsNullOrEmpty(salesDeliverySaveReq.Ations.ToString()))
             {
-                if (salesOrderSaveReq.Ations == OrderAtion.Draft)
+                if (salesDeliverySaveReq.Ations == OrderAtion.Draft)
                 {
                     result = _serviceSaleOrderApp.WorkflowBuild(jobname, FuncID, UserID, job_data, Model.Remark, int.Parse(Model.SboId), Model.CardCode, Model.CardName, jobDocTotal, int.Parse(Model.billBaseType), int.Parse(Model.billBaseEntry), "BOneAPI", className);
                 }
-                if (salesOrderSaveReq.Ations == OrderAtion.Submit)
+                if (salesDeliverySaveReq.Ations == OrderAtion.Submit)
                 {
                     result = _serviceSaleOrderApp.WorkflowBuild(jobname, FuncID, UserID, job_data, Model.Remark, int.Parse(Model.SboId), Model.CardCode, Model.CardName, jobDocTotal, int.Parse(Model.billBaseType), int.Parse(Model.billBaseEntry), "BOneAPI", className);
                     if (int.Parse(result) > 0)
@@ -100,18 +76,18 @@ namespace OpenAuth.App.Order
                     }
 
                 }
-                if (salesOrderSaveReq.Ations == OrderAtion.Resubmit)
+                if (salesDeliverySaveReq.Ations == OrderAtion.Resubmit)
                 {
                     if (jobdata == "1")
                     {
-                        if (bool.Parse(_serviceSaleOrderApp. UpdateAudit(salesOrderSaveReq.JobId, job_data, Model.Remark, Model.DocTotal, Model.CardCode, Model.CardName)))
+                        if (bool.Parse(_serviceSaleOrderApp.UpdateAudit(salesDeliverySaveReq.JobId, job_data, Model.Remark, Model.DocTotal, Model.CardCode, Model.CardName)))
                         {
-                            result = _serviceSaleOrderApp.WorkflowSubmit(salesOrderSaveReq.JobId, UserID, Model.Remark, "", 0);
+                            result = _serviceSaleOrderApp.WorkflowSubmit(salesDeliverySaveReq.JobId, UserID, Model.Remark, "", 0);
                         }
                     }
                     else
                     {
-                        result = _serviceSaleOrderApp.WorkflowSubmit(salesOrderSaveReq.JobId, UserID, Model.Remark, "", 0);
+                        result = _serviceSaleOrderApp.WorkflowSubmit(salesDeliverySaveReq.JobId, UserID, Model.Remark, "", 0);
                     }
                 }
             }
@@ -203,6 +179,52 @@ namespace OpenAuth.App.Order
                 }
             }
             return res > 0 ? true : false;
+        }
+        /// <summary>
+        /// 交货详情主数据
+        /// </summary>
+        /// <param name="DocNum"></param>
+        /// <param name="SboId"></param>
+        /// <param name="tablename"></param>
+        /// <param name="ViewCustom"></param>
+        /// <param name="ViewSales"></param>
+        /// <returns></returns>
+        public async Task<DataTable> QuerySaleDetailsNew(string DocNum, int SboId, string tablename, bool ViewCustom, bool ViewSales)
+        {
+            string U_YWY = string.Empty;
+            if (await IsExistMySql(tablename, "U_YWY"))
+            {
+                U_YWY = ",U_YWY";
+            }
+            string strSql = string.Format("SELECT CardCode,IF(" + ViewCustom + ",CardName,'******' ) AS CardName,IF(" + ViewCustom + ",CntctCode,0) AS CntctCode,IF(" + ViewCustom + ",NumAtCard,'******' ) AS NumAtCard,IF(" + ViewCustom + ",DocCur,'') AS DocCur,IF(" + ViewCustom + ",DocRate,0) AS DocRate");
+            strSql += string.Format(",DocNum,DocType,IF(" + ViewSales + ",DocTotal,0) AS DocTotal,DocDate,DocDueDate,TaxDate,SupplCode,ShipToCode,PayToCode,Address,Address2,Comments,SlpCode,TrnspCode,GroupNum,PeyMethod,VatPercent,LicTradNum,Indicator,PartSupply,ReqDate,CANCELED");
+            strSql += string.Format(",DpmPrcnt,Printed,DocStatus,OwnerCode,U_FPLB,U_SL,U_YGMD{0},sbo_id as SboId", U_YWY);
+            string[] tabArray = tablename.Split(new char[] { '_' });
+            if (tabArray[0].ToString() != "buy")
+            {
+                strSql += string.Format(",U_CallID,U_CallName,U_SerialNumber ");
+            }
+            if (tabArray[0].ToString() == "sale")
+            {
+                if (tabArray.Length > 1 && tabArray[1].ToString() == "orin")
+                {
+                    strSql += string.Format(",U_New_ORDRID");
+                }
+                strSql += string.Format(",IF(" + ViewSales + ",DocTotalFC,0) AS DocTotalFC,IF(" + ViewSales + ",DiscSum,0) AS DiscSum,IF(" + ViewSales + ",DiscSumFC,0) AS DiscSumFC,IF(" + ViewSales + ",DiscPrcnt,0) AS DiscPrcnt");
+            }
+            strSql += string.Format(" FROM {0}." + tablename + "", "nsap_bone");
+            strSql += string.Format(" WHERE DocEntry={0} AND sbo_id={1}", DocNum, SboId);
+            return UnitWork.ExcuteSqlTable(ContextType.NsapBaseDbContext, strSql, CommandType.Text, null);
+        }
+        public async Task<bool> IsExistMySql(string tablename, string filename)
+        {
+            bool result = false;
+            string strSql = string.Format("SELECT COUNT(*) FROM information_schema.columns WHERE table_schema='nsap_bone' AND table_name ='{0}' AND column_name='{1}'", tablename, filename);
+            object obj = UnitWork.ExecuteScalar(ContextType.NsapBaseDbContext, strSql, CommandType.Text, null);
+            if (obj.ToString() == "0" || obj.ToString() == null)
+            { result = false; }
+            else { result = true; }
+            return result;
         }
     }
 }
--- OpenAuth.Core/OpenAuth.WebApi/Controllers/Order/SalesDeliveryController.cs
+++ OpenAuth.Core/OpenAuth.WebApi/Controllers/Order/SalesDeliveryController.cs
@@ -19,7 +19,7 @@ namespace OpenAuth.WebApi.Controllers.Order
     /// <summary>
     /// 销售交货
     /// </summary>
-    [Route("api/order/[controller]")]
+    [Route("api/Order/[controller]")]
     [ApiController]
     [ApiExplorerSettings(GroupName = "Order")]
     public class SalesDeliveryController : Controller
@@ -47,7 +47,7 @@ namespace OpenAuth.WebApi.Controllers.Order
         /// 添加销售交货
         /// </summary>
         [HttpPost]
-        public async Task<Response<string>> SalesDeliverySave(SalesOrderSaveReq salesOrderSaveReq)
+        public async Task<Response<string>> SalesDeliverySave(SalesDeliverySaveReq salesDeliverySaveReq)
         {
             var result = new Response<string>();
             var UserID = _serviceBaseApp.GetUserNaspId();
@@ -55,18 +55,14 @@ namespace OpenAuth.WebApi.Controllers.Order
             try
             {
                 string funcId = "0"; string logstring = ""; string jobname = "";
-
                 funcId = _serviceSaleOrderApp.GetJobTypeByAddress("sales/SalesDelivery.aspx");
                 logstring = "根据销售订单下销售交货单"; jobname = "销售交货";
-
-                //break;
-                result.Result = await _salesDeliveryApp.SalesDeliverySave(salesOrderSaveReq, UserID, int.Parse(funcId), "0", jobname, SboID, "0");
-
+                result.Result = await _salesDeliveryApp.SalesDeliverySave(salesDeliverySaveReq, UserID, int.Parse(funcId), "0", jobname, SboID, "0");
                 return result;
             }
             catch (Exception ex)
             {
-                Log.Logger.Error($"地址：{Request.Path}，参数：{salesOrderSaveReq}， 错误：{ex.Message}");
+                Log.Logger.Error($"地址：{Request.Path}，参数：{salesDeliverySaveReq}， 错误：{ex.Message}");
                 throw new Exception("导出失败！" + ex.ToString());
             }
         }
@@ -79,7 +75,7 @@ namespace OpenAuth.WebApi.Controllers.Order
         [Route("GridDataBind")]
         public TableData GridDataBind(SalesDeliveryListReq salesDeliveryListReq)
         {
-            var TableData=new TableData();
+            var TableData = new TableData();
             int rowCount = 0;
             var UserID = _serviceBaseApp.GetUserNaspId();
             var SboID = _serviceBaseApp.GetUserNaspSboID(UserID);
@@ -96,13 +92,13 @@ namespace OpenAuth.WebApi.Controllers.Order
             {
                 if (isOpen == "0")
                 {
-                    TableData.Data= _serviceSaleOrderApp.SelectBillViewInfo(out  rowCount,salesDeliveryListReq.limit, salesDeliveryListReq.page, salesDeliveryListReq.qtype, salesDeliveryListReq.sortname, salesDeliveryListReq.sortorder, type, true, true, UserID, SboID, true, DepID, true, true);
+                    TableData.Data = _serviceSaleOrderApp.SelectBillViewInfo(out rowCount, salesDeliveryListReq.limit, salesDeliveryListReq.page, salesDeliveryListReq.qtype, salesDeliveryListReq.sortname, salesDeliveryListReq.sortorder, type, true, true, UserID, SboID, true, DepID, true, true);
                     TableData.Count = rowCount;
                 }
                 else
                 {
-                    //默认接口
-                    TableData.Data= _serviceSaleOrderApp.SelectBillListInfo(out rowCount, salesDeliveryListReq.limit, salesDeliveryListReq.page, salesDeliveryListReq.query, salesDeliveryListReq.sortname, salesDeliveryListReq.sortorder, type, true, true, UserID, SboID, true, DepID, true, true, sqlcont, sboname);
+                    //默认接口 
+                    TableData.Data = _serviceSaleOrderApp.SelectBillListInfo(out rowCount, salesDeliveryListReq.limit, salesDeliveryListReq.page, salesDeliveryListReq.query, salesDeliveryListReq.sortname, salesDeliveryListReq.sortorder, type, true, true, UserID, SboID, true, DepID, true, true, sqlcont, sboname);
                     TableData.Count = rowCount;
                 }
             }
@@ -114,5 +110,103 @@ namespace OpenAuth.WebApi.Controllers.Order
             return TableData;
         }
         #endregion
+        #region 销售交货详情
+        /// <summary>
+        /// 销售交货详情
+        /// </summary>
+        /// <returns></returns>
+        //[HttpPost]
+        //[Route("QuerySaleDetailsNew")]
+        //public async Task<TableData> QuerySaleDetailsNew(QuerySaleDetailsNewReq querySaleDetailsNewReq)
+        //{
+        //    var result = new TableData();
+        //    DataTable dtTable = await _salesDeliveryApp.QuerySaleDetailsNew(querySaleDetailsNewReq.DocNum, int.Parse(querySaleDetailsNewReq.SboId), querySaleDetailsNewReq.tablename, querySaleDetailsNewReq.ViewCustom, querySaleDetailsNewReq.ViewSales);
+        //    if (dtTable.Rows.Count == 0) return null;
+        //    string _manager = NSAP.Data.Sales.BillDelivery.DropPopupOwnerCodeNew(SboId, dtTable.Rows[0][31].ToString()).DataTableToJSON();
+        //    string _sales = NSAP.Data.Sales.BillDelivery.DropPopupSlpCodeNew(SboId, dtTable.Rows[0][18].ToString()).DataTableToJSON();
+        //    string _mark = NSAP.Data.Sales.BillDelivery.DropPopupIndicatorNew(SboId, dtTable.Rows[0][24].ToString()).DataTableToJSON();
+        //    string _shipType = NSAP.Data.Sales.BillDelivery.DropPopupTrnspCodeNew(SboId, dtTable.Rows[0][19].ToString()).DataTableToJSON();
+        //    string _paymentCond = NSAP.Data.Sales.BillDelivery.GetGroupNumNew(SboId, dtTable.Rows[0][20].ToString()).DataTableToJSON();
+        //    string _andbuy = NSAP.Data.Sales.BillDelivery.DropPopupSlpCodeNew(SboId, dtTable.Rows[0][35].ToString()).DataTableToJSON();
+        //    string _docCur = NSAP.Data.Sales.BillDelivery.DropPopupDocCurNew(dtTable.Rows[0][4].ToString(), SboId).DataTableToJSON();
+        //    string _cntctCode = NSAP.Data.Sales.BillDelivery.DropPopupCntctPrsnNew(dtTable.Rows[0][0].ToString(), SboId, isCopy == "1" ? "0" : dtTable.Rows[0][2].ToString()).DataTableToJSON();
+        //    string _shipToCode = NSAP.Data.Sales.BillDelivery.GetAddressNew("S", dtTable.Rows[0][0].ToString(), SboId).DataTableToJSON();
+        //    string _payToCode = NSAP.Data.Sales.BillDelivery.GetAddressNew("B", dtTable.Rows[0][0].ToString(), SboId).DataTableToJSON();
+        //    string _balance = NSAP.Data.Sales.BillDelivery.SelectBalanceNew(dtTable.Rows[0][0].ToString(), SboId.ToString());
+        //    string _balanceS = NSAP.Data.Sales.BillDelivery.GetSumBalDueNew(dtTable.Rows[0][18].ToString(), "C", SboId.ToString());
+        //    DataTable _lineTable = new DataTable(); //NSAP.Data.Sales.BillDelivery.GetItemCodeListNew(DocNum, linename, ViewSales, SboId);
+        //    if (tablename.ToString().ToLower() == "buy_opor" || tablename.ToString().ToLower() == "opor")
+        //    {
+        //        _lineTable = NSAP.Data.Sales.BillDelivery.GetItemCodeListPur(DocNum, linename, ViewSales, SboId);
+        //    }
+        //    else
+        //    {
+        //        _lineTable = NSAP.Data.Sales.BillDelivery.GetItemCodeListNew(DocNum, linename, ViewSales, SboId);
+        //    }
+        //    //获得关联报价单的物料成本
+        //    DataTable _stockInfo = NSAP.Data.Sales.BillDelivery.GetCallInfoById(DocNum, SboId.ToString(), tablename, "0", "stock");
+        //    if (_stockInfo.Rows.Count > 0 && _stockInfo.Rows[0][0].ToString() != "")
+        //    {
+        //        for (int i = 0; i < _lineTable.Rows.Count; i++)
+        //        {
+        //            for (int j = 0; j < _stockInfo.Rows.Count; j++)
+        //            {
+        //                if (_lineTable.Rows[i][1].ToString() == _stockInfo.Rows[j][1].ToString())
+        //                {
+        //                    _lineTable.Rows[i][9] = _stockInfo.Rows[j][2];
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    string _line = _lineTable.DataTableToJSON();
+        //    string _customNew = NSAP.Data.Sales.BillDelivery.QuerySalesCustomNew(DocNum, SboId, tablename).DataRowByIndexToJSON(0);
+        //    string _storehouse = NSAP.Data.Sales.BillDelivery.DropPopupWhsCodeNew(SboId, "0").DataTableToJSON();
+        //    string _attachmentData = "";
+        //    if (!string.IsNullOrEmpty(funcID))
+        //    {
+        //        int fileType = int.Parse(NSAP.Data.Sales.BillDelivery.GetattchtypeByfuncid(int.Parse(funcID)).Rows[0]["type_id"].ToString());
+        //        _attachmentData = NSAP.Data.Sales.BillDelivery.GetFilesList(DocNum, fileType.ToString(), SboId).DataTableToJSON();
+        //    }
+
+        //    //服务呼叫
+        //    DataTable _callInfo = NSAP.Data.Sales.BillDelivery.GetCallInfoById(DocNum, SboId.ToString(), tablename, "0", "call");
+        //    string U_CallID = "", U_CallName = "", U_SerialNumber = "";
+        //    if (_callInfo.Rows.Count > 0 && _callInfo.Rows[0][0].ToString() != "")
+        //    {
+        //        U_CallID = _callInfo.Rows[0][0].ToString();
+        //        U_CallName = _callInfo.Rows[0][1].ToString();
+        //        U_SerialNumber = _callInfo.Rows[0][2].ToString();
+        //    }
+        //    if ((tablename == "sale_ordn" || tablename == "sale_orin") && !string.IsNullOrEmpty(dtTable.Rows[0][37].ToString()))
+        //    {
+        //        U_CallID = dtTable.Rows[0][37].ToString();
+        //        U_CallName = dtTable.Rows[0][38].ToString();
+        //        U_SerialNumber = dtTable.Rows[0][39].ToString();
+        //    }
+
+        //    string _main = dtTable.DataRowByIndexToJSON(0);
+        //    StringBuilder sMain = new StringBuilder();
+        //    var billSalesName = string.Empty;
+        //    if (dtTable.Rows[0][7].ToString() == "I")
+        //    {
+        //        billSalesName = "billSalesDetails";
+        //    }
+        //    else
+        //    {
+        //        billSalesName = "billSalesAcctCode";
+        //    }
+        //    sMain.AppendFormat("{0},\"WhsCode\":\"{3}\",\"" + billSalesName + "\":{1},\"CustomFields\":{2},\"attachmentData\":{4},\"U_CallID\":\"{5}\",\"U_CallName\":\"{6}\",\"U_SerialNumber\":\"{7}\"", _main.TrimEnd('}'), _line, _customNew, _lineTable.Rows[0]["WhsCode"].ToString(), _attachmentData, U_CallID, U_CallName, U_SerialNumber);
+        //    sMain.Append("}");
+        //    StringBuilder sBuilder = new StringBuilder("{");
+        //    sBuilder.AppendFormat("\"manager\":{0},\"sales\":{1},\"mark\":{2},\"shipType\":{3},\"storehouse\":{14},\"paymentCond\":{4},\"paymentMode\":{5},\"andbuy\":{6},\"docCur\":{7},\"cntctCode\":{8},\"balance\":{9},\"balanceS\":{10},\"main\":{11},\"shipToCode\":{12},\"payToCode\":{13}",
+        //        _manager, _sales, _mark, _shipType, _paymentCond, "[]", _andbuy, _docCur, _cntctCode, _balance == "" ? "0" : _balance, _balanceS == "" ? "0" : _balanceS, sMain, _shipToCode, _payToCode, _storehouse
+        //    );
+        //    sBuilder.Append("}");
+        //    return sBuilder.ToString();
+        //}
+        #endregion
+
+
     }
 }
