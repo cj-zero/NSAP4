64943e3674cb215c9dd24fd3b800f74a2134037e
mulunlei <mulunlei@126.com>
2022-02-16 10:53:15 +0800

    修正判断条件，申请人有权限查看金额

--- OpenAuth.Core/OpenAuth.App/Serve/BeforeSaleDemandApp.cs
+++ OpenAuth.Core/OpenAuth.App/Serve/BeforeSaleDemandApp.cs
@@ -146,7 +146,7 @@ namespace OpenAuth.App
             var result = detail.MapTo<BeforeSaleDemandResp>();
             //判断当前用户是否有查看金额信息的权限 默认否false
             result.IsShowAmount = false;
-            if (loginContext.User.CreateId==result.CreateUserId|| loginContext.User.Account == Define.SYSTEM_USERNAME|| loginContext.Roles.Any(c => c.Name.Equal("需求反馈审批-销售总助"))|| loginContext.Roles.Any(c => c.Name.Equal("需求反馈审批-研发总助"))|| loginContext.Roles.Any(c => c.Name.Equal("总经理")))
+            if (result.CreateUserId == loginContext.User.Id || loginContext.User.Account == Define.SYSTEM_USERNAME || loginContext.Roles.Any(c => c.Name.Equal("需求反馈审批-销售总助")) || loginContext.Roles.Any(c => c.Name.Equal("需求反馈审批-研发总助")) || loginContext.Roles.Any(c => c.Name.Equal("总经理")))
             {
                 result.IsShowAmount = true;
             }
@@ -541,7 +541,7 @@ namespace OpenAuth.App
                             TestUserName = req.BeforeSaleProSchedulings.Find(x => x.Stage == 2).UserName,
                             FlowInstanceId = beforeSaleDemand.FlowInstanceId,//流程id
                             //需要研发实施，实施负责人为研发负责人
-                            ExecutorUserId = beforeSaleDemand.IsDevDeploy.HasValue&&beforeSaleDemand.IsDevDeploy.Value == 1 ? req.BeforeSaleProSchedulings.Find(x => x.Stage == 1).UserId : "",
+                            ExecutorUserId = beforeSaleDemand.IsDevDeploy.HasValue && beforeSaleDemand.IsDevDeploy.Value == 1 ? req.BeforeSaleProSchedulings.Find(x => x.Stage == 1).UserId : "",
                             ExecutorName = beforeSaleDemand.IsDevDeploy.HasValue && beforeSaleDemand.IsDevDeploy.Value == 1 ? req.BeforeSaleProSchedulings.Find(x => x.Stage == 1).UserName : "",
                             CreateTime = DateTime.Now,
                             CreateUserId = loginContext.User.Id,
