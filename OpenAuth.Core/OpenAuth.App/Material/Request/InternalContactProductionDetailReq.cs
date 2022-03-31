b911f4eb47978f4ed9a1d36cd55f016160347202
xinweilicong <1229346271@qq.com>
2022-03-31 12:16:59 +0800

    fix:内联单v3.6

---
+++ OpenAuth.Core/OpenAuth.App/Material/Request/InternalContactProductionDetailReq.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.AutoMapper;
+using OpenAuth.Repository.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAuth.App.Material.Request
+{
+    [AutoMapTo(typeof(InternalContactProductionDetail))]
+    public class InternalContactProductionDetailReq
+    {
+        public string InternalContactProductionId { get; set; }
+        /// <summary>
+        /// 序列号
+        /// </summary>
+        public string MnfSerial { get; set; }
+        public string WhsCode { get; set; }
+        public string OrgName { get; set; }
+        public int? Quantity { get; set; }
+    }
+}
