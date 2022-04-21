4320f8c51b45565ca23019afc0e3bcace29fd1ba
新威研选 <522183622@qq.com>
2022-04-21 16:46:31 +0800

    表结构调整

--- OpenAuth.Core/OpenAuth.Repository/Domain/Customer/CustomerList.cs
+++ OpenAuth.Core/OpenAuth.Repository/Domain/Customer/CustomerList.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * @author : Eaven
  * @date : 2022-4-20
- * @desc :  客户黑\白名单
+ * @desc :  客户
  */
 using OpenAuth.Repository.Core;
 using System;
@@ -14,7 +14,7 @@ using System.Text;
 namespace OpenAuth.Repository.Domain
 {
     /// <summary>
-    /// 客户黑\白名单
+    /// 客户
     /// </summary>
     [Table("customer_list")]
     public class CustomerList : BaseEntity<int>
@@ -23,12 +23,14 @@ namespace OpenAuth.Repository.Domain
         /// <summary>
         /// 客户Id;客户Id
         /// </summary>
+        [Column("Customer_Id")]
         [Description("客户Id")]
         public int CustomerId { get; set; }
 
         /// <summary>
         /// 客户编码;客户编码
         /// </summary>
+        [Column("Customer_No")]
         [Required(ErrorMessage = "客户编码不能为空")]
         [MaxLength(20)]
         [Description("客户编码")]
@@ -37,20 +39,30 @@ namespace OpenAuth.Repository.Domain
         /// <summary>
         /// 客户名称
         /// </summary>
+        [Column("Customer_Name")]
         [Required(ErrorMessage = "客户名称不能为空")]
         [MaxLength(20)]
         [Description("客户名称")]
         public string CustomerName { get; set; }
 
         /// <summary>
+        /// 客户来源;客户来源
+        /// </summary>
+        [Column("Customer_Source")]
+        [MaxLength(200)]
+        public string CustomerSource { get; set; }
+
+        /// <summary>
         /// 业务员Id
         /// </summary>
+        [Column("Saler_Id")]
         [Description("业务员Id")]
         public int SalerId { get; set; }
 
         /// <summary>
         /// 业务员名称
         /// </summary>
+        [Column("Saler_Name")]
         [Required(ErrorMessage = "业务员名称不能为空")]
         [MaxLength(20)]
         [Description("业务员名称")]
@@ -59,12 +71,14 @@ namespace OpenAuth.Repository.Domain
         /// <summary>
         /// 部门Id
         /// </summary>
+        [Column("Department_Id")]
         [Description("部门Id")]
         public int DepartmentId { get; set; }
 
         /// <summary>
         /// 部门
         /// </summary>
+        [Column("Department_Name")]
         [Required(ErrorMessage = "部门不能为空")]
         [MaxLength(20)]
         [Description("部门")]
@@ -75,6 +89,26 @@ namespace OpenAuth.Repository.Domain
         /// </summary>
         [Description("类型")]
         public int Type { get; set; }
+
+        /// <summary>
+        /// 标签Id;1：未报价，2：已成交，3：公海领取，4：即将掉入公海
+        /// </summary>
+        [Column("Labe_Index")]
+        [Description("标签Id")]
+        public int LabeIndex { get; set; }
+
+        /// <summary>
+        /// 标签;未报价、已成交、公海领取、即将掉入公海
+        /// </summary>
+        [Description("标签")]
+        public string Labe { get; set; }
+
+        /// <summary>
+        /// 订单类型;1：未报价，2：已成交
+        /// </summary>
+        [Column("Order_Type")]
+        [Description("订单类型")]
+        public int OrderType { get; set; }
 
         /// <summary>
         /// 备注
