e9e59d21cc75330846289ccd3357ba7d11e24bd5
licong <1229346271@qq.com>
2022-04-25 11:05:30 +0800

    fix:查询修改

--- OpenAuth.Core/OpenAuth.App/Serve/ServiceOrderApp.cs
+++ OpenAuth.Core/OpenAuth.App/Serve/ServiceOrderApp.cs
@@ -1807,7 +1807,8 @@ namespace OpenAuth.App
             });
             data.ForEach(c =>
             {
-                c.MaterialTypes = c.ServiceWorkOrders.Select(s => s.MaterialCode == "无序列号" ? "无序列号" : (s.MaterialCode.Substring(0, s.MaterialCode.IndexOf("-")) == "" ? "无序列号" : s.MaterialCode.Substring(0, s.MaterialCode.IndexOf("-")))).ToList();
+                c.MaterialTypes = c.ServiceWorkOrders.Select(s => s.MaterialCode.IndexOf("-") == -1 ? "无序列号" : s.MaterialCode.Substring(0, s.MaterialCode.IndexOf("-"))).Distinct().ToList();
+                //c.MaterialTypes = c.ServiceWorkOrders.Select(s => s.MaterialCode == "无序列号" ? "无序列号" : (s.MaterialCode.Substring(0, s.MaterialCode.IndexOf("-")) == "" ? "无序列号" : s.MaterialCode.Substring(0, s.MaterialCode.IndexOf("-")))).Distinct().ToList();
             });
             result.Data = data;
             result.Count = query.Count();
