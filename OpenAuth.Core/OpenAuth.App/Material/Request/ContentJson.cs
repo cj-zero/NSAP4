bc18466698f78df6868a0f4757f3e811bcf06fff
licong <1229346271@qq.com>
2022-06-22 11:30:59 +0800

    fix:ecn

---
+++ OpenAuth.Core/OpenAuth.App/Material/Request/ContentJson.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAuth.App.Material
+{
+    public class ContentJson
+    {
+        public string materialCode { get; set; }
+        public string selectList { get; set; }
+        public List<ChangeContent> changeContent { get; set; }
+        public string preMaterial { get; set; }
+        public string postMaterial { get; set; }
+        public string preNums { get; set; }
+        public string postNums { get; set; }
+        public string unit { get; set; }
+        public List<FileList> fileList { get; set; }
+        public string modification { get; set; }
+    }
+
+    public class ChangeContent
+    {
+        public string title { get; set; }
+        public string type { get; set; }
+        public bool isNumber { get; set; }
+        public string value { get; set; }
+    }
+    public class FileList
+    {
+        public string url { get; set; }
+    }
+}
