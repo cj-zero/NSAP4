e55840bb1e48d3171fb86492cf2fe10d897d1ef0
yuhuiwen <2505741046@qq.com>
2022-07-26 15:42:01 +0800

    新增新威课堂模块

---
+++ OpenAuth.Core/NSAP.App.WebApi/Controllers/NewareClassroom/CompulsoryCourseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NSAP.App.WebApi.Controllers
+{
+    /// <summary>
+    /// 必修课
+    /// </summary>
+    [ApiController]
+    public class CompulsoryCourseController : BaseController
+    {
+
+    }
+}
