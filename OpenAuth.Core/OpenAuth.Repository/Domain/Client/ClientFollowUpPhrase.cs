20ada2de498c69ef216b21f4fac273d996f4a04c
新威研选 <522183622@qq.com>
2022-08-05 18:05:10 +0800

    客户管理模糊查询修改，客户详情页跟进模块

---
+++ OpenAuth.Core/OpenAuth.Repository/Domain/Client/ClientFollowUpPhrase.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * @author : wangying
+ * @date : 2022-8-5
+ * @desc : 常用语
+ */
+using OpenAuth.Repository.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace OpenAuth.Repository.Domain
+{
+    /// <summary>
+    /// 常用语
+    /// </summary>
+    [Table("client_followup_phrase")]
+    public class ClientFollowUpPhrase : BaseEntity<int>
+    {
+        /// <summary>
+        /// 业务员编码
+        /// </summary>
+        public int SlpCode { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Remark { get; set; }
+        
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public string CreateUser { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime? CreateDate { get; set; }
+      
+        public override void GenerateDefaultKeyVal()
+        {
+        }
+
+        public override bool KeyIsNull()
+        {
+            return Id == 0;
+        }
+    }
+}
