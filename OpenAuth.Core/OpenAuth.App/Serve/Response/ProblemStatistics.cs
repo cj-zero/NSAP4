af53df5d0262975a897acd08e1062dcc5fea5b3e
cj <395821867@qq.com>
2022-08-18 14:30:10 +0800

    首页报表

---
+++ OpenAuth.Core/OpenAuth.App/Serve/Response/ProblemStatistics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAuth.App.Serve.Response
+{
+    public class ProblemStatisticsMin
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int Num { get; set; }
+    }
+    public class ProblemStatisticsMax: ProblemStatisticsMin
+    {
+        public string Id { get; set; }
+        public List<ProblemStatisticsMin> Children { get; set; }
+    }
+    public class DailyReport
+    {
+        public string Id { get; set; }
+        public string description { get; set; }
+        public string code { get; set; }
+
+    }
+}
