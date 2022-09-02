61be646e6075377ee5fdb9dad66076f3b8d78782
xiarenchun <neware.com.cn@DELL-5000>
2022-09-02 18:35:16 +0800

    销售报价单无效联系人提交时再次验证

--- OpenAuth.Core/OpenAuth.WebApi/Controllers/Order/OrderDraftController.cs
+++ OpenAuth.Core/OpenAuth.WebApi/Controllers/Order/OrderDraftController.cs
@@ -568,7 +568,15 @@ namespace OpenAuth.WebApi.Controllers.Order
                 {
                     orderReq.Order.Remark = orderReq.Order.Remark.Replace("'", "''");
                 }
-                result.Result = _serviceSaleOrderApp.Save(orderReq);
+
+                string msg = _serviceSaleOrderApp.Save(orderReq);
+                if (msg.Contains("-201"))
+                {
+                    result.Result = msg.Split('-')[0].ToString();
+                    result.Code = 201;
+                }
+
+                result.Result = msg;
             }
             catch (Exception ex)
             {
