7f16f72b4b3cafccad250479570dbb8546ad4123
licong <1229346271@qq.com>
2022-09-13 18:34:53 +0800

    fix:获取烤机结果接口修改

---
+++ OpenAuth.Core/OpenAuth.App/Nwcali/Models/CheckResultDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAuth.App.Nwcali
+{
+    public class CheckResultDto
+    {
+        public string TaskId { get; set; }
+        public int LastTime { get; set; }
+        public int Status { get; set; }
+        public int ErrCount { get; set; }
+        public List<CheckResultItem> CheckItems { get; set; }
+    }
+
+    public class CheckResultItem
+    {
+        public string CheckId { get; set; }
+        public int CheckType { get; set; }
+        public int LastTime { get; set; }
+        public int Status { get; set; }
+        public int ErrCount { get; set; }
+        public List<CheckResultItemRecord> Records { get; set; }
+    }
+
+    public class CheckResultItemRecord
+    {
+        public int BegSeq { get; set; }
+        public int EndSeq { get; set; }
+        public string Err { get; set; }
+        public int CheckTime { get; set; }
+    }
+}
