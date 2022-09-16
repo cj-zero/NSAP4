08dcaeb8b4aee2910e316ab46ed7fcd5a78b5940
licong <1229346271@qq.com>
2022-09-16 16:15:44 +0800

    fix:内联单修改

--- OpenAuth.Core/OpenAuth.App/Material/InternalContactApp.cs
+++ OpenAuth.Core/OpenAuth.App/Material/InternalContactApp.cs
@@ -301,8 +301,8 @@ namespace OpenAuth.App.Material
                         obj.IW = single.IW;
                         obj.FlowInstanceId = single.FlowInstanceId;
                         obj.CreateTime = DateTime.Now;
-                        obj.CreateUserId = obj.CreateUserId;
-                        obj.CreateUser = obj.CreateUser;
+                        obj.CreateUserId = single.CreateUserId;
+                        obj.CreateUser = single.CreateUser;
 
                         #region 子表删除重新添加
                         if (obj.InternalContactAttchments.Count > 0)
