100d797abf6513488ac86ad505cc9fb553b4f502
licong <1229346271@qq.com>
2022-09-16 11:06:24 +0800

    fix:烤机状态逻辑修改

--- OpenAuth.Core/OpenAuth.App/Nwcali/Models/CheckResultDto.cs
+++ OpenAuth.Core/OpenAuth.App/Nwcali/Models/CheckResultDto.cs
@@ -7,7 +7,7 @@ namespace OpenAuth.App.Nwcali
     public class CheckResultDto
     {
         public string TaskId { get; set; }
-        public int LastTime { get; set; }
+        public int? LastTime { get; set; }
         public int Status { get; set; }
         public int ErrCount { get; set; }
         public List<CheckResultItem> CheckItems { get; set; }
@@ -17,7 +17,7 @@ namespace OpenAuth.App.Nwcali
     {
         public string CheckId { get; set; }
         public int CheckType { get; set; }
-        public int LastTime { get; set; }
+        public int? LastTime { get; set; }
         public int Status { get; set; }
         public int ErrCount { get; set; }
         public List<CheckResultItemRecord> Records { get; set; }
@@ -25,9 +25,9 @@ namespace OpenAuth.App.Nwcali
 
     public class CheckResultItemRecord
     {
-        public int BegSeq { get; set; }
-        public int EndSeq { get; set; }
+        public int? BegSeq { get; set; }
+        public int? EndSeq { get; set; }
         public string Err { get; set; }
-        public int CheckTime { get; set; }
+        public int? CheckTime { get; set; }
     }
 }
