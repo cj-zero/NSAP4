90a8653d829ebe9d2a03c36dce5ad33e099b9196
Antony <wanghaodong@neware.com.cn>
2022-09-30 17:49:34 +0800

    客户（中间商与终端）关系操作 公海领取客户，分配客户  Alter ClientRelationModule

--- OpenAuth.Core/OpenAuth.App/Customer/CustomerListApp.cs
+++ OpenAuth.Core/OpenAuth.App/Customer/CustomerListApp.cs
@@ -19,6 +19,7 @@ using System.Collections;
 using System.Data;
 using OpenAuth.App.Order;
 using NSAP.Entity.Client;
+using OpenAuth.App.ClientRelation;
 
 namespace OpenAuth.App.Customer
 {
@@ -27,12 +28,14 @@ namespace OpenAuth.App.Customer
         private readonly UserManagerApp _userManagerApp;
         private readonly ServiceSaleOrderApp _serviceSaleOrderApp;
         private readonly ServiceBaseApp _serviceBaseApp;
+        private readonly ClientRelationApp _clientRelationApp;
         public CustomerListApp(IUnitWork unitWork, IAuth auth,
-            UserManagerApp userManagerApp, ServiceSaleOrderApp serviceSaleOrderApp, ServiceBaseApp serviceBaseApp) : base(unitWork, auth)
+            UserManagerApp userManagerApp, ServiceSaleOrderApp serviceSaleOrderApp,ClientRelationApp clientRelationApp, ServiceBaseApp serviceBaseApp) : base(unitWork, auth)
         {
             _userManagerApp = userManagerApp;
             _serviceSaleOrderApp = serviceSaleOrderApp;
             _serviceBaseApp = serviceBaseApp;
+            _clientRelationApp = clientRelationApp;
         }
 
         /// <summary>
@@ -861,6 +864,19 @@ namespace OpenAuth.App.Customer
                             await UnitWork.AddAsync<CustomerSalerHistory>(history);
                             //领取后,将客户从公海中移出
                             await UnitWork.DeleteAsync<CustomerList>(c => c.CustomerNo == item.CustomerNo);
+
+                            //更新客户关系
+                            await _clientRelationApp.ResignRelations(new ClientRelation.Request.ResignRelReq { 
+                                jobid = Convert.ToInt32(job_id),
+                                userid = userId.ToString(),
+                                username = loginUser.Name,
+                                ClientNo = client.CardCode,
+                                flag = client.is_reseller=="Y"?1:0,
+                                OperateType= client.is_reseller == "Y"?4:5,
+                                job_userid = userId.ToString(),
+                                job_username = loginUser.Name
+                            });
+
                             await UnitWork.SaveAsync();
                         }
                     }
@@ -1097,6 +1113,20 @@ namespace OpenAuth.App.Customer
                             await UnitWork.AddAsync<CustomerSalerHistory>(history);
                             //领取后,将客户从公海中移出
                             await UnitWork.DeleteAsync<CustomerList>(c => c.CustomerNo == item.CustomerNo);
+
+                            //更新客户关系
+                            await _clientRelationApp.ResignRelations(new ClientRelation.Request.ResignRelReq
+                            {
+                                jobid = Convert.ToInt32(job_id),
+                                userid = userId.ToString(),
+                                username = loginUser.Name,
+                                ClientNo = client.CardCode,
+                                flag = client.is_reseller == "Y" ? 1 : 0,
+                                OperateType = client.is_reseller == "Y" ? 4 : 5,
+                                job_userid = req.SlpCode.ToString(),
+                                job_username = req.SlpName
+                            });
+
                             await UnitWork.SaveAsync();
                         }
                     }
