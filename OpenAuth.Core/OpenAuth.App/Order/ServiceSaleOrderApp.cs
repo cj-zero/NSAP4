92f5bfc19fc9fa81d17b19aa721665011dd0b553
xiarenchun <neware.com.cn@DELL-5000>
2022-10-11 14:22:28 +0800

    拟取消订单

--- OpenAuth.Core/OpenAuth.App/Order/ServiceSaleOrderApp.cs
+++ OpenAuth.Core/OpenAuth.App/Order/ServiceSaleOrderApp.cs
@@ -591,7 +591,23 @@ SELECT a.type_id FROM nsap_oa.file_type a LEFT JOIN nsap_base.base_func b ON a.f
                                 thisinfo.OrderLastDate = DateTime.Now;
                                 thisinfo.FirstCreateDate = DateTime.Now;
                                 //设置报价单提交
-                                result = Eshop_OrderStatusFlow(thisinfo, billDelivery.billSalesDetails, orderReq.Order.U_New_ORDRID);
+                                if (string.IsNullOrEmpty(orderReq.Order.U_New_ORDRID))
+                                {
+                                    result = Eshop_OrderStatusFlow(thisinfo, billDelivery.billSalesDetails, 0);
+                                }
+                                else
+                                {
+                                    if (orderReq.Order.U_New_ORDRID.Contains(","))
+                                    {
+                                        string[] orderids = orderReq.Order.U_New_ORDRID.Split(',');
+                                        result = Eshop_OrderStatusFlow(thisinfo, billDelivery.billSalesDetails, Convert.ToInt32(orderids[0]));
+                                    }
+                                    else
+                                    {
+                                        result = Eshop_OrderStatusFlow(thisinfo, billDelivery.billSalesDetails, Convert.ToInt32(orderReq.Order.U_New_ORDRID));
+                                    }
+                                }
+                               
                                 #endregion
                             }
                             else { result = "0"; }
@@ -633,7 +649,22 @@ SELECT a.type_id FROM nsap_oa.file_type a LEFT JOIN nsap_base.base_func b ON a.f
                             thisinfo.OrderLastDate = DateTime.Now;
                             thisinfo.FirstCreateDate = DateTime.Now;
                             //设置报价单提交
-                            result = Eshop_OrderStatusFlow(thisinfo, billDelivery.billSalesDetails, orderReq.Order.U_New_ORDRID);
+                            if (string.IsNullOrEmpty(orderReq.Order.U_New_ORDRID))
+                            {
+                                result = Eshop_OrderStatusFlow(thisinfo, billDelivery.billSalesDetails, 0);
+                            }
+                            else
+                            {
+                                if (orderReq.Order.U_New_ORDRID.Contains(","))
+                                {
+                                    string[] orderids = orderReq.Order.U_New_ORDRID.Split(',');
+                                    result = Eshop_OrderStatusFlow(thisinfo, billDelivery.billSalesDetails, Convert.ToInt32(orderids[0]));
+                                }
+                                else
+                                {
+                                    result = Eshop_OrderStatusFlow(thisinfo, billDelivery.billSalesDetails, Convert.ToInt32(orderReq.Order.U_New_ORDRID));
+                                }
+                            }
                             #endregion
                         }
                         else { result = "0"; }
