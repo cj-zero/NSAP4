fe96e946bfb995a30fb78a34fd1c5d1975612f30
Antony <wanghaodong@neware.com.cn>
2022-10-13 10:30:29 +0800

    客户（中间商与终端）关系操作修改 V4.3 客户审核通过后同步关系定时任务添加

---
+++ OpenAuth.Core/OpenAuth.App/Jobs/ClientRelationJob.cs
@@ -0,0 +1,34 @@
+﻿using OpenAuth.App.ClientRelation;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAuth.App.Jobs
+{
+    /// <summary>
+    /// 客户审核通过后同步关系
+    /// </summary>
+    public class ClientRelationJob : IJob
+    {
+        private readonly OpenJobApp _openJobApp;
+        private readonly ClientRelationApp _clientRelationApp;
+
+        public ClientRelationJob(OpenJobApp openJobApp, ClientRelationApp  clientRelationApp)
+        {
+            _openJobApp = openJobApp;
+            _clientRelationApp = clientRelationApp;
+        }
+
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var jobId = context.MergedJobDataMap.GetString(Define.JOBMAPKEY);
+            await _clientRelationApp.SyncRelations();
+            _openJobApp.RecordRun(jobId);
+        }
+
+
+    }
+}
