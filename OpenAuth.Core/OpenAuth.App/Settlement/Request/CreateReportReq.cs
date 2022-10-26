8ad6d1eb2244ab50052031990b78dce7e25c42d6
licong <1229346271@qq.com>
2022-10-26 17:09:21 +0800

    fix:提成报表

---
+++ OpenAuth.Core/OpenAuth.App/Settlement/Request/CreateReportReq.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAuth.App.Settlement.Request
+{
+    public class CreateReportReq
+    {
+        public string Name { get; set; }
+        public List<int> Ids { get; set; }
+    }
+
+    public class OutsourcCondition
+    {
+        public int? Id { get; set; }
+        public int? OutsourcReportId { get; set; }
+        public string StatusName { get; set; }
+    }
+
+    public class TechnicianGrades
+    {
+        public int AppUserId { get; set; }
+        public int GradeName { get; set; }
+    }
+}
