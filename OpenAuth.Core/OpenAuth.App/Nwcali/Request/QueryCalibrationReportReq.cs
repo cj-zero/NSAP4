d4e9ede31f88d096c675d6979c6c35c2b1b9dcd2
yuhuiwen <2505741046@qq.com>
2022-10-27 18:20:42 +0800

    新增校准报表

---
+++ OpenAuth.Core/OpenAuth.App/Nwcali/Request/QueryCalibrationReportReq.cs
@@ -0,0 +1,30 @@
+﻿using OpenAuth.App.Request;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAuth.App.Nwcali.Request
+{
+    /// <summary>
+    /// 校准明细报表
+    /// </summary>
+    public class QueryCalibrationReportReq:PageReq
+    {
+        /// <summary>
+        /// 校准开始时间
+        /// </summary>
+        public DateTime? StartTime { get; set; }
+        /// <summary>
+        /// 校准开始时间
+        /// </summary>
+        public DateTime? EndTime { get; set; }
+        /// <summary>
+        /// 部门id
+        /// </summary>
+        public string OrgId { get; set; }
+        /// <summary>
+        /// 操作人
+        /// </summary>
+        public string Operator { get; set; }
+    }
+}
