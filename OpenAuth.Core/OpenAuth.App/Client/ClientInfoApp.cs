a94d48210dea4e16a71e05adabb10ae8c64bab52
Antony <wanghaodong@neware.com.cn>
2022-10-31 11:51:50 +0800

    相似客户sql语句修复 fix error v5

--- OpenAuth.Core/OpenAuth.App/Client/ClientInfoApp.cs
+++ OpenAuth.Core/OpenAuth.App/Client/ClientInfoApp.cs
@@ -2177,7 +2177,7 @@ namespace OpenAuth.App.Client
             pubSql += "D.Phone1,D.Cellular,D.Balance,D.U_Name,D.U_EndCustomerName,D.U_EndCustomerContact,{0} FROM {1}.crm_OCRD D ";
             pubSql += "LEFT JOIN {1}.crm_OCRY Y ON Y.Code=D.MailCountr LEFT JOIN {1}.crm_OCST T ON T.Code=D.State2 ";
             pubSql += "LEFT JOIN {1}.crm_OSLP P ON P.SlpCode=D.SlpCode AND P.sbo_id=D.sbo_id ";
-            string strSimilarity = "0 AS Similarity1, 0 AS Similarity2, 0 AS Similarity3, 0 AS Similarity4, 0 AS Similarity5, 0 AS Similarity6, 0 AS Similarity7, 0 AS Similarity8, 0 AS Similarity9, 0 AS Similarity10,0 AS Similarity11,0 AS Similarity12,  case  when Flag is null then 0 ELSE Flag END as IsFlag , ClueId   ";
+            string strSimilarity = "0 AS Similarity1, 0 AS Similarity2, 0 AS Similarity3, 0 AS Similarity4, 0 AS Similarity5, 0 AS Similarity6, 0 AS Similarity7, 0 AS Similarity8, 0 AS Similarity9, 0 AS Similarity10,0 AS Similarity11,0 AS Similarity12 ";
 
             StringBuilder strSql = new StringBuilder();
             strSql.Append("SELECT   sbo_id,CardCode,CardName,CardFName,SlpName,CntctPrsn,Address,Phone1,Cellular,Balance,U_Name,U_EndCustomerName,U_EndCustomerContact,");
@@ -2187,7 +2187,7 @@ namespace OpenAuth.App.Client
             }
             else
             {
-                strSql.Append("Similarity1, Similarity2, Similarity3, Similarity4, Similarity5, Similarity6, Similarity7, Similarity8, Similarity9, Similarity10,Similarity11,Similarity12,DfTcnician, case  when Flag is null then 0 ELSE Flag END as IsFlag , ClueId  FROM ( ");
+                strSql.Append("Similarity1, Similarity2, Similarity3, Similarity4, Similarity5, Similarity6, Similarity7, Similarity8, Similarity9, Similarity10,Similarity11,Similarity12,DfTcnician FROM ( ");
             }
           
 
@@ -2275,7 +2275,7 @@ namespace OpenAuth.App.Client
             {  //搜索当前业务伙伴相似
                 strSql.Append("SELECT sbo_id,E.CardCode,CardName,CardFName,SlpName,CntctPrsn,Address,Phone1,Cellular,Balance,U_Name,U_EndCustomerName,U_EndCustomerContact,");
                 strSql.Append("SUM(Similarity1) AS Similarity1,SUM(Similarity2) AS Similarity2,SUM(Similarity3) AS Similarity3,SUM(Similarity4) AS Similarity4,SUM(Similarity5) AS Similarity5,");
-                strSql.Append("SUM(Similarity6) AS Similarity6,SUM(Similarity7) AS Similarity7,SUM(Similarity8) AS Similarity8,SUM(Similarity9) AS Similarity9,SUM(Similarity10) AS Similarity10,SUM(Similarity11) AS Similarity11,SUM(Similarity12) AS Similarity12,DfTcnician,   Y.Flag , Z.SerialNumber as ClueId  ");
+                strSql.Append("SUM(Similarity6) AS Similarity6,SUM(Similarity7) AS Similarity7,SUM(Similarity8) AS Similarity8,SUM(Similarity9) AS Similarity9,SUM(Similarity10) AS Similarity10,SUM(Similarity11) AS Similarity11,SUM(Similarity12) AS Similarity12,DfTcnician ");
                 strSql.Append(" FROM (");
                 #region CardName
                 //==
@@ -2404,8 +2404,8 @@ namespace OpenAuth.App.Client
                 }
                 #endregion
                 strSql.Append(" ) AS E ");
-                strSql.Append(" LEFT JOIN  (SELECT c.Id, c.SubNo ,c.ClientNo,c.Flag , c.IsActive, c.ParentNo, c.IsDelete, c.ScriptFlag,ROW_NUMBER() OVER (PARTITION BY ClientNo ORDER BY CreateDate  DESC) rn from erp4.clientrelation c)   Y ON Y.ClientNo = E.CardCode AND Y.Flag !=2  AND Y.IsActive =1 AND Y.ScriptFlag =0 AND   Y.rn = 1  AND  Y.IsDelete = 0    ");
-                strSql.Append("  LEFT JOIN  (SELECT SerialNumber,CardCode from  erp4_serve.clue) Z  ON Z.CardCode = E.CardCode      ");
+                //strSql.Append(" LEFT JOIN  (SELECT c.Id, c.SubNo ,c.ClientNo,c.Flag , c.IsActive, c.ParentNo, c.IsDelete, c.ScriptFlag,ROW_NUMBER() OVER (PARTITION BY ClientNo ORDER BY CreateDate  DESC) rn from erp4.clientrelation c)   Y ON Y.ClientNo = E.CardCode AND Y.Flag !=2  AND Y.IsActive =1 AND Y.ScriptFlag =0 AND   Y.rn = 1  AND  Y.IsDelete = 0    ");
+                //strSql.Append("  LEFT JOIN  (SELECT SerialNumber,CardCode from  erp4_serve.clue) Z  ON Z.CardCode = E.CardCode      ");
                 strSql.Append("  LEFT JOIN (SELECT GROUP_CONCAT(b.`Name`) as multiname ,GROUP_CONCAT(b.Tel1) as multiTel,GROUP_CONCAT(b.Cellolar) as multiCell ,a.CardCode  from nsap_bone.crm_OCRD a left join nsap_bone.crm_ocpr b on a.CardCode = b.CardCode  GROUP BY a.CardCode)  k on k.CardCode = E.CardCode      ");
                 strSql.Append("  LEFT JOIN (SELECT GROUP_CONCAT(b.Address) as multiAddress ,GROUP_CONCAT(b.Building) as multiBuilding ,a.CardCode  from nsap_bone.crm_OCRD a left join nsap_bone.crm_crd1 b on a.CardCode = b.CardCode  GROUP BY a.CardCode)  T on T.CardCode = E.CardCode    ");
 
@@ -2523,9 +2523,23 @@ namespace OpenAuth.App.Client
             DataTable datatable = UnitWork.ExcuteSqlTable(ContextType.NsapBaseDbContext, strSql.ToString(), CommandType.Text, null);
             for (int i = 0; i < datatable.Rows.Count; i++)
             {
-               var info = GetUserOrgInfo("", datatable.Rows[i]["SlpName"].ToString());
+                var info = GetUserOrgInfo("", datatable.Rows[i]["SlpName"].ToString());
                 datatable.Rows[i]["SlpName"] = info == null ? datatable.Rows[i]["SlpName"] : info.FirstOrDefault().OrgName + "-" + datatable.Rows[i]["SlpName"];
             }
+            #region 
+            datatable.Columns.Add("IsFlag", typeof(int));
+            datatable.Columns.Add("ClueId", typeof(String));
+            var clientNoList = datatable.AsEnumerable().Select(row => row.Field<string>("CardCode")).ToList();  //CardCode
+            var clueList = UnitWork.Find<OpenAuth.Repository.Domain.Serve.Clue>(a=>clientNoList.Contains(a.CardCode)).ToList();
+            var flagList = UnitWork.Find<OpenAuth.Repository.Domain.ClientRelation>(a => clientNoList.Contains(a.ClientNo) && a.IsActive == 1 && a.ScriptFlag == 0 && a.Flag != 2 && a.IsDelete == 0).ToList();
+            foreach (var datarow in datatable.AsEnumerable())
+            {
+                var specClue = clueList.Where(a => a.CardCode == datarow["CardCode"].ToString()).FirstOrDefault();
+                var specFlag = flagList.Where(a => a.ClientNo == datarow["CardCode"].ToString()).FirstOrDefault();
+                datarow["IsFlag"] = specFlag == null ? 0 : specFlag.Flag;
+                datarow["ClueId"] = specClue==null? "": specClue.SerialNumber;
+            }
+            #endregion
             return datatable;
         }
         #endregion
