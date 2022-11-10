127e6b9bbf4f747db37721525085c700ffbe98b4
Antony <wanghaodong@neware.com.cn>
2022-11-10 16:06:43 +0800

    fix clue update module

--- OpenAuth.Core/OpenAuth.App/Clue/ModelDto/ClueInfoDto.cs
+++ OpenAuth.Core/OpenAuth.App/Clue/ModelDto/ClueInfoDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenAuth.App.Clue.Request;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -80,34 +81,7 @@ namespace OpenAuth.App.Clue.ModelDto
         ///备注
         /// </summary>
         public string Remark { get; set; }
-        /// <summary>
-        /// 联系人名称
-        /// </summary>
-        public string Name { get; set; }
-        /// <summary>
-        /// 联系方式1
-        /// </summary>
-        public string Tel1 { get; set; }
-        /// <summary>
-        /// 详细地址(国家省市)
-        /// </summary>
-        public string Address1 { get; set; }
-        /// <summary>
-        /// 详细地址
-        /// </summary>
-        public string Address2 { get; set; }
-        /// <summary>
-        /// 角色（0：决策者、1：普通人）
-        /// </summary>
-        public string Role { get; set; }
-        /// <summary>
-        ///职位
-        /// </summary>
-        public string Position { get; set; }
-        /// <summary>
-        /// 邮箱
-        /// </summary>
-        public string Email { get; set; }
+        public List<ContPerson> ContPerList { get; set; }
     }
     /// <summary>
     /// 操作记录
