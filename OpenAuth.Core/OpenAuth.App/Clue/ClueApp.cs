b06c972647f2d7b3ecc61c49ed9b50e96194f9ad
Antony <wanghaodong@neware.com.cn>
2022-11-14 11:21:57 +0800

    fix clue issut v1.0

--- OpenAuth.Core/OpenAuth.App/Clue/ClueApp.cs
+++ OpenAuth.Core/OpenAuth.App/Clue/ClueApp.cs
@@ -420,23 +420,27 @@ namespace OpenAuth.App
             };
             var data = UnitWork.Add<OpenAuth.Repository.Domain.Serve.Clue, int>(clue);
             UnitWork.Save();
-            OpenAuth.Repository.Domain.Serve.ClueContacts cluecontacts = new Repository.Domain.Serve.ClueContacts
+            #region multiple contact person info
+            List<ClueContacts> clueContacts = new List<ClueContacts>();
+            foreach (var contactItem in addClueReq.ContPerList)
             {
-                ClueId = data.Id,
-                Name = addClueReq.Name,
-                Tel1 = addClueReq.Tel1,
-                Role = addClueReq.Role,
-                Email = addClueReq.Email,
-                Position = addClueReq.Position,
-                Address1 = addClueReq.Address1,
-                Address2 = addClueReq.Address2,
-                CreateTime = DateTime.Now,
-                CreateUser = loginUser.Name,
-                IsDefault = true
-            };
-            UnitWork.Add<OpenAuth.Repository.Domain.Serve.ClueContacts, int>(cluecontacts);
-            UnitWork.Save();
+                clueContacts.Add(new Repository.Domain.Serve.ClueContacts {
+                    ClueId = data.Id,
+                    Name = contactItem.Name,
+                    Tel1 = contactItem.Tel1,
+                    Email = contactItem.Email,
+                    Position = contactItem.Position,
+                    Address2 = contactItem.Address,
+                    CreateTime = DateTime.Now,
+                    CreateUser = loginUser.Name,
+                    IsDefault = contactItem.IsDefault,
+                    Status = contactItem.IsActive ? 1 : 0
+                });
+            }
 
+            UnitWork.BatchAdd<OpenAuth.Repository.Domain.Serve.ClueContacts, int>(clueContacts.ToArray());
+            UnitWork.Save();
+            #endregion
             var log = new AddClueLogReq();
             log.ClueId = data.Id;
             log.LogType = 0;
@@ -656,8 +660,9 @@ namespace OpenAuth.App
                             Address2 = contactItem.Address,
                             CreateTime = DateTime.Now,
                             CreateUser = loginUser.Name,
-                            IsDefault = contactItem.IsDefault
-                        });
+                            IsDefault = contactItem.IsDefault,
+                            Status = contactItem.IsActive ? 1 : 0
+                        }); ;
                     }
 
                     foreach (var ditem in emodel)
