dc9e5ffa3a170bf9d0fac0882239c133ae83df7d
Antony <wanghaodong@neware.com.cn>
2022-11-15 15:18:33 +0800

    Add ManageModule Initial

---
+++ OpenAuth.Core/OpenAuth.App/Material/ManageAccBindApp.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using OpenAuth.App.ClientRelation;
+using OpenAuth.App.Interface;
+using OpenAuth.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAuth.App.Material
+{
+    public class ManageAccBindApp : OnlyUnitWorkBaeApp
+    {
+        ServiceBaseApp _serviceBaseApp;
+        private ILogger<ManageAccBindApp> _logger;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="unitWork"></param>
+        /// <param name="auth"></param>
+        public ManageAccBindApp(IUnitWork unitWork, IAuth auth, ILogger<ManageAccBindApp> logger, ServiceBaseApp serviceBaseApp) : base(unitWork, auth)
+        {
+            _serviceBaseApp = serviceBaseApp;
+            _logger = logger;
+        }
+
+
+
+
+    }
+
+}
---
+++ OpenAuth.Core/OpenAuth.WebApi/Controllers/Material/ManageAccBindController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAuth.App.ClientRelation;
+using OpenAuth.App.Material;
+
+namespace OpenAuth.WebApi.Controllers.Material
+{
+    /// <summary>
+    /// 工程部账号绑定相关
+    /// </summary>
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "Material")]
+    public class ManageAccBindController : ControllerBase
+    {
+        private readonly ManageAccBindApp _app;
+        /// <summary>
+        /// construct func
+        /// </summary>
+        /// <param name="app"></param>
+        public ManageAccBindController(ManageAccBindApp app)
+        {
+            _app = app;
+        }
+
+
+
+    }
+}
