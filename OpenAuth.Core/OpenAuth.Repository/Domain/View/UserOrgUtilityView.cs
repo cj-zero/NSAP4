5e41771ef4826df2c1dd2e91fcfc78ec50b04ed6
Antony <wanghaodong@neware.com.cn>
2022-11-16 12:01:33 +0800

    Add ManageModule about account binding utility

--- OpenAuth.Core/OpenAuth.Repository/Domain/View/UserOrgUtilityView.cs
+++ OpenAuth.Core/OpenAuth.Repository/Domain/View/UserOrgUtilityView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenAuth.Repository.Domain.Material;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
@@ -18,4 +19,85 @@ namespace OpenAuth.Repository.Domain.View
 
 
     }
+
+    public class UserManageUtilityRsp
+    {
+        public int Count { get; set; }
+
+        public List<UserManageUtilityView> muv { get; set; } = new List<UserManageUtilityView>();
+    }
+
+
+    public class UserManageUtilityView
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Account { get; set; }
+
+        public string deptName { get; set; }
+
+        public int Status { get; set; }
+    }
+
+    public class UserManageUtilityRequest
+    {
+
+        public string SlpName { get; set; }
+
+        public string deptName { get; set; }
+
+        public int page { get; set; }
+
+        public int Limit { get; set; }
+    }
+
+    public class BindUtilityRequest
+    {
+
+        public int page { get; set; }
+
+        public int Limit { get; set; }
+
+        public string query { get; set; }
+    }
+
+    public class BindUtilityUpdateRequest
+    {
+
+        public string MAccount { get; set; }
+
+        public string MName { get; set; }
+
+        public string LAccount { get; set; }
+
+        public string LName { get; set; }
+
+        public string  Level { get; set; }
+
+        public int  DutyFlag { get; set; }
+        public int IsDelete { get; set; }
+    }
+
+
+    public class BindUtilityRep
+    {
+
+        public int Count { get; set; }
+
+        public List<ManageAccountBind> mab { get; set; } = new List<ManageAccountBind>();
+    }
+
+
+    public class MaterialUsers
+    {
+        public int UserID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string FirstNameAndLastName { get; set; }
+    }
+
+
 }
