8892c708232499c02d91443aa1206e9b2a6e92fd
Antony <wanghaodong@neware.com.cn>
2022-11-18 11:25:40 +0800

    Add ManageModule about chart and export v1.0

--- OpenAuth.Core/OpenAuth.Repository/Domain/View/UserOrgUtilityView.cs
+++ OpenAuth.Core/OpenAuth.Repository/Domain/View/UserOrgUtilityView.cs
@@ -1,4 +1,7 @@
-﻿using OpenAuth.Repository.Domain.Material;
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using Magicodes.ExporterAndImporter.Core;
+using Magicodes.ExporterAndImporter.Excel;
+using OpenAuth.Repository.Domain.Material;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -80,6 +83,73 @@ namespace OpenAuth.Repository.Domain.View
         public int IsDelete { get; set; }
     }
 
+
+    public class DutyChartRequest
+    { 
+        public int Month { get; set; }
+    }
+
+    public class DutyChartResponse
+    {
+        public List<string> XData { get; set; }
+        public List<SerieData> YData { get; set; } = new List<SerieData>();
+    }
+
+    public class SerieData
+    {
+        public string Name { get; set; }
+        public List<double> SerieVal { get; set; }= new List<double>();
+    }
+
+    public class RateTableResponse
+    {
+        public string RateLevel { get; set; }
+        public string Name { get; set; }
+        public int FinishedTask { get; set; }
+        public int DueTask { get; set; }
+        public int ExcelTask { get; set; }
+
+    }
+
+    public class RateTableReq
+    {
+        public List<RateTableExport>  texports =new List<RateTableExport>();
+    }
+
+    [ExcelExporter(Name = "评分表", TableStyle = "Light10", AutoFitAllColumn = true, MaxRowNumberOnASheet = 2)]
+    public class RateTableExport
+    {
+        [ExporterHeader(DisplayName = "岗位级别")]
+        public string DutyLevel { get; set; }
+        [ExporterHeader(DisplayName = "姓名")]
+        public string Name { get; set; }
+        [ExporterHeader(DisplayName = "图纸完成量(满分70分)")]
+        public string FinishedTask { get; set; }
+        [ExporterHeader(DisplayName = "按时完成(满分15分)")]
+        public string DueTask { get; set; }
+        [ExporterHeader(DisplayName = "超额完成(满分10分)")]
+        public string ExcelTask { get; set; }
+        [ExporterHeader(DisplayName = "岗位贡献(满分5分)")]
+        public string DutyContribute { get; set; }
+        [ExporterHeader(DisplayName = "分数")]
+        public string Score { get; set; }
+        [ExporterHeader(DisplayName = "生产评分")]
+        public string RateScore { get; set; }
+        [ExporterHeader(DisplayName = "总分")]
+        public string TotalScore { get; set; }
+        [ExporterHeader(DisplayName = "等级")]
+        public string RateLevel { get; set; }
+    }
+
+    public class SerieManageData
+    {
+        public string Owner { get; set; }
+
+        public int Total { get; set; }
+
+        public int CompleteCount { get; set; }
+
+    }
 
     public class BindUtilityRep
     {
