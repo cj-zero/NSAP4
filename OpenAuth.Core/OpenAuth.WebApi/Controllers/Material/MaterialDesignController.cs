78ac4a62a9f507cf5eaa227c4edaed996b64dd7f
新威研选 <522183622@qq.com>
2022-11-21 14:59:22 +0800

    WMS对接注释添加

--- OpenAuth.Core/OpenAuth.WebApi/Controllers/Material/MaterialDesignController.cs
+++ OpenAuth.Core/OpenAuth.WebApi/Controllers/Material/MaterialDesignController.cs
@@ -441,9 +441,17 @@ namespace OpenAuth.WebApi.Controllers.Material
         #endregion
 
         #region WMS接口对接
+        /// <summary>
+        /// WMS接口对接
+        /// </summary>
+        /// <param name="ProductNo"></param>
+        /// <param name="ItemName"></param>
+        /// <returns></returns>
         [HttpGet]
         public DataTable PostWMSFileUrl(string ProductNo, string ItemName)
         {
+            //参数：生产订单号，图纸编码
+            //返回：版本号、图纸文件路径、样机 / 批量
             string sql = string.Format(@" select n.VersionNo,n.FileUrl, n.IsDemo
                                          from erp4_serve.manage_screening n
                                          left join nsap_bone.product_owor s on n.DocEntry = s.OriginNum and n.ItemCode = s.ItemCode where 1 = 1");
