e88d64d2c4f11ffebd9f3dae85c3dd03a340c632
cj <395821867@qq.com>
2022-12-01 16:18:28 +0800

    个人历史信息时间变更

--- OpenAuth.Core/OpenAuth.App/Serve/ReimburseInfoApp.cs
+++ OpenAuth.Core/OpenAuth.App/Serve/ReimburseInfoApp.cs
@@ -3093,7 +3093,7 @@ namespace OpenAuth.App
 
         }
 
-        public async Task<TableData> HistoryReimburseInfoForUser_new(QueryReimburseInfoListReq req)
+        public async Task<TableData> HistoryReimburseInfoForUser(QueryReimburseInfoListReq req)
         {
             var loginContext = _auth.GetCurrentUser();
             if (loginContext == null)
@@ -3240,8 +3240,8 @@ namespace OpenAuth.App
             {
                 #region 提成
                 var CommissionInfos = from a in UnitWork.Find<CommissionOrder>(r => req.CreateUserId == r.CreateUserId && r.ApprovalStatus >= 3)
-                               .WhereIf(!string.IsNullOrWhiteSpace(req.StartDate.ToString()), c => c.PayTime >= req.StartDate.Value)
-                               .WhereIf(!string.IsNullOrWhiteSpace(req.EndDate.ToString()), c => c.PayTime <= req.EndDate.Value.AddDays(1).Date)
+                               .WhereIf(!string.IsNullOrWhiteSpace(req.StartDate.ToString()), c => c.UpdateTime >= req.StartDate.Value)
+                               .WhereIf(!string.IsNullOrWhiteSpace(req.EndDate.ToString()), c => c.UpdateTime <= req.EndDate.Value.AddDays(1).Date)
                                .WhereIf(req.PayStatus == 1, c => c.ApprovalStatus == 3)
                                .WhereIf(req.PayStatus == 2, c => c.ApprovalStatus == 4)
                                       select new HistoryDetailForUserResp
@@ -3250,7 +3250,7 @@ namespace OpenAuth.App
                                           Type = 1,
                                           ServiceOrderId = a.ServiceOrderId,
                                           TotalMoney = a.Amount,
-                                          PayTime = a.PayTime,
+                                          PayTime = a.UpdateTime,
                                           FlowInstanceId = a.FlowInstanceId,
                                           Status = a.Status
                                           //   c.ActivityType,
@@ -3266,8 +3266,8 @@ namespace OpenAuth.App
                 var ReimburseInfos = from a in UnitWork.Find<ReimburseInfo>(r => req.CreateUserId == r.CreateUserId && r.RemburseStatus >= 7 && r.RemburseStatus <= 9)
                                   .WhereIf(req.PayStatus == 1, c => c.RemburseStatus != 9)
                                   .WhereIf(req.PayStatus == 2, c => c.RemburseStatus == 9)
-                                  .WhereIf(!string.IsNullOrWhiteSpace(req.StartDate.ToString()), c => c.PayTime >= req.StartDate.Value)
-                                  .WhereIf(!string.IsNullOrWhiteSpace(req.EndDate.ToString()), c => c.PayTime <= req.EndDate.Value.AddDays(1).Date)
+                                  .WhereIf(!string.IsNullOrWhiteSpace(req.StartDate.ToString()), c => c.UpdateTime >= req.StartDate.Value)
+                                  .WhereIf(!string.IsNullOrWhiteSpace(req.EndDate.ToString()), c => c.UpdateTime <= req.EndDate.Value.AddDays(1).Date)
                                      select new HistoryDetailForUserResp
                                      {
                                          Id = a.MainId,
@@ -3275,7 +3275,7 @@ namespace OpenAuth.App
                                          Type = 2,
                                          ServiceOrderId = a.ServiceOrderId,
                                          TotalMoney = a.TotalMoney,
-                                         PayTime = a.PayTime,
+                                         PayTime = a.UpdateTime,
                                          FlowInstanceId = a.FlowInstanceId,
                                          Status = a.RemburseStatus,
                                          StatusName = a.RemburseStatus == 9 ? "已支付" : "待支付"
@@ -3291,8 +3291,8 @@ namespace OpenAuth.App
             {
                 #region 结算/代理
                 var OutsourcInfos = (from a in UnitWork.Find<Outsourc>(r => req.CreateUserId == r.CreateUserId)
-                                     .WhereIf(!string.IsNullOrWhiteSpace(req.StartDate.ToString()), c => c.PayTime >= req.StartDate.Value)
-                                     .WhereIf(!string.IsNullOrWhiteSpace(req.EndDate.ToString()), c => c.PayTime <= req.EndDate.Value.AddDays(1).Date)
+                                     .WhereIf(!string.IsNullOrWhiteSpace(req.StartDate.ToString()), c => c.UpdateTime >= req.StartDate.Value)
+                                     .WhereIf(!string.IsNullOrWhiteSpace(req.EndDate.ToString()), c => c.UpdateTime <= req.EndDate.Value.AddDays(1).Date)
                                      .Include(c => c.OutsourcExpenses)
                                      .Where(a => a.OutsourcExpenses.Count() > 0)
                                          //  join c in UnitWork.Find<FlowInstance>(c => c.CustomName == "个人代理结算单") on a.FlowInstanceId equals c.Id
@@ -3302,7 +3302,7 @@ namespace OpenAuth.App
                                          Type = 3,
                                          ServiceOrderId = a.OutsourcExpenses.FirstOrDefault().ServiceOrderId,
                                          TotalMoney = a.TotalMoney,
-                                         PayTime = a.PayTime,
+                                         PayTime = a.UpdateTime,
                                          FlowInstanceId = a.FlowInstanceId
 
                                          //  c.ActivityType,
@@ -3732,7 +3732,7 @@ namespace OpenAuth.App
         /// </summary>
         /// <param name="req"></param>
         /// <returns></returns>
-        public async Task<TableData> HistoryReimburseInfoForUser(QueryReimburseInfoListReq req)
+        public async Task<TableData> HistoryReimburseInfoForUser_old(QueryReimburseInfoListReq req)
         {
             var loginContext = _auth.GetCurrentUser();
             if (loginContext == null)
