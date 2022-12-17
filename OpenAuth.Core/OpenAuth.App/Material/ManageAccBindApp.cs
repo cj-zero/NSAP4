4fba7d7b5e319fab2741d2ed3550ca038a6aea82
Antony <wanghaodong@neware.com.cn>
2022-12-17 10:56:01 +0800

    filter the deleted manage material

--- OpenAuth.Core/OpenAuth.App/Material/ManageAccBindApp.cs
+++ OpenAuth.Core/OpenAuth.App/Material/ManageAccBindApp.cs
@@ -453,12 +453,12 @@ select b._System_objNBS , b.fld005787 as itemcode ,b.fld017268 as num ,b.idRecor
 union all
 select c._System_objNBS ,c.fld005879 as itemcode ,c.fld005878 as num ,c.idRecord,c.RecordGuid ,c.deleted,c._System_Progress,c.CreatedDate,c.DateModified from (select * from OBJ163 where idRecord in (select max(idRecord) from OBJ163 group by _System_objNBS)) c
 union all
-select d._System_objNBS ,d.fld005719 as itemcode ,d.fld005717 as num ,d.idRecord,d.RecordGuid ,d.deleted,d._System_Progress,d.CreatedDate,d.DateModified from (select * from OBJ169 where idRecord in (select max(idRecord) from OBJ169 group by _System_objNBS)) d) as t WHERE t.itemcode in   ( {0} )  and  t.num like  '%{1}%' ", String.Join(",", req.Alpha.Select(i => $"'{i.Replace("\'", "\"")}'")), req.ProjectNo);
+select d._System_objNBS ,d.fld005719 as itemcode ,d.fld005717 as num ,d.idRecord,d.RecordGuid ,d.deleted,d._System_Progress,d.CreatedDate,d.DateModified from (select * from OBJ169 where idRecord in (select max(idRecord) from OBJ169 group by _System_objNBS)) d) as t WHERE  t.deleted = 0  and  t.itemcode in   ( {0} )  and  t.num like  '%{1}%' ", String.Join(",", req.Alpha.Select(i => $"'{i.Replace("\'", "\"")}'")), req.ProjectNo);
             //sql += req;
             var specJobList = UnitWork.ExcuteSql<BetaView>(ContextType.ManagerDbContext, sql.ToString(), CommandType.Text, null);
             foreach (var item in specJobList)
             {
-                var sql2 = string.Format(@"  	SELECT TaskId, TaskNBS, Subject, '{1}' as ProjectNo  from Tasks t where t.CaseRecGuid = '{0}'  ", item.RecordGuid,item._System_objNBS);
+                var sql2 = string.Format(@"  	SELECT TaskId, TaskNBS, Subject, '{1}' as ProjectNo  from Tasks t where t.isDeleted=0  and  t.CaseRecGuid = '{0}'  ", item.RecordGuid,item._System_objNBS);
                 var taskNbsList = UnitWork.ExcuteSql<TaskNbsView>(ContextType.ManagerDbContext, sql2.ToString(), CommandType.Text, null);
                 modeldata.AddRange(taskNbsList);
             }
@@ -473,7 +473,7 @@ select d._System_objNBS ,d.fld005719 as itemcode ,d.fld005717 as num ,d.idRecord
 t.AssignDate,t.CreatedDate,t.AssignedBy,t.CaseRecGuid,t.RecordGuid,t.TaskNBS,t.TaskOwnerId,t.TimeAllocated,CompletedDate as CompleteTime,u.FirstNameAndLastName as ownername,DueHours=DATEDIFF(hh,case when t.isFinished=1 then  ISNULL(t.CompletedDate,getdate()) else GETDATE() end,t.duedate) , WorkHours=DATEDIFF(hh,t.StartDate,t.DueDate) ,AssignedTo=ISNULL(STUFF((select  ', '+ ISNULL(us.FirstName,us.UserName)+ISNULL(' '+us.LastName,'')   from TaskAssignment as ta join UsersAndGroups as us on us.UserID=ta.UserId where ta.TaskId=t.taskId   FOR XML PATH('')),1,2,''),ISNULL(us2.FirstName + ISNULL(' ' + us2.LastName,''),us2.UserName))  from Tasks t
 left JOIN UsersAndGroups u on u.UserID = t.TaskOwnerId
 left outer join UsersAndGroups as us2 on us2.UserID=t.OwnerId
-    WHERE TaskNBS    in   ( {0} )  ", String.Join(",", req.Alpha.Select(i => $"'{i}'")));
+    WHERE TaskNBS    in   ( {0} )  and  t.isDeleted=0  ", String.Join(",", req.Alpha.Select(i => $"'{i}'")));
             //sql += req;
             var modeldata = UnitWork.ExcuteSql<statisticsTableB>(ContextType.ManagerDbContext, sql, CommandType.Text, null).ToList();
             result.Data = modeldata.Where(a=>a.Subject.Contains(req.ProjectNo)).ToList();
