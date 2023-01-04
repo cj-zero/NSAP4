ce9f066faa75ce80af410bb36d9d3fae1e221499
cj <395821867@qq.com>
2023-01-04 11:53:03 +0800

    技术员代码找回

---
+++ OpenAuth.Core/OpenAuth.WebApi/Controllers/Serve/TechnicianController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAuth.App.Response;
+using OpenAuth.App.Serve;
+using OpenAuth.App.Serve.Request;
+using System.Threading.Tasks;
+
+namespace OpenAuth.WebApi.Controllers.Serve
+{
+    /// <summary>
+    /// 技术员
+    /// </summary>
+    [Route("api/serve/[controller]/[action]")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "Serve")]
+    public class TechnicianController : ControllerBase
+    {
+        private readonly TechnicianApp _technicianApp;
+        public TechnicianController(TechnicianApp technicianApp)
+        {
+            _technicianApp = technicianApp;
+
+        }
+
+        /// <summary>
+        /// 获取技术员列表
+        /// </summary>
+        /// <param name="req"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<TableData> GetTechnicianList([FromQuery] QueryTechnicianListReq req)
+        {
+            return await _technicianApp.GetTechnicianList(req);
+        }
+        /// <summary>
+        /// 获取技术员服务单信息
+        /// </summary>
+        /// <param name="req"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<TableData> GetTechnicianOrder(string Id)
+        {
+            return await _technicianApp.GetTechnicianOrder(Id);
+        }
+
+
+
+
+
+
+    }
+}
