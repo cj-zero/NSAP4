692fc381b1cc02d3f57462d22da900675287a4a3
xiarenchun <neware.com.cn@DELL-5000>
2023-01-04 14:31:31 +0800

    fix: 提交给我的查询

--- OpenAuth.Core/OpenAuth.WebApi/Controllers/Order/OrderWorkbenchController.cs
+++ OpenAuth.Core/OpenAuth.WebApi/Controllers/Order/OrderWorkbenchController.cs
@@ -60,7 +60,7 @@ namespace OpenAuth.WebApi.Controllers.Order
                 viewSales = powers.ViewSales;
                 viewCustom = true;
             }
-            DataTable dt = _orderWorkbenchApp.GetSubmtToMe(orderSubmtToMeReq.limit, orderSubmtToMeReq.page, orderSubmtToMeReq.query, orderSubmtToMeReq.sortname, orderSubmtToMeReq.sortorder, UserID, orderSubmtToMeReq.types, orderSubmtToMeReq.Applicator, orderSubmtToMeReq.Customer, orderSubmtToMeReq.Status, orderSubmtToMeReq.BeginDate, orderSubmtToMeReq.EndDate, ViewCustom: viewCustom, ViewSales: viewSales, out rowCount);
+            DataTable dt = _orderWorkbenchApp.GetSubmtToMe(orderSubmtToMeReq.limit, orderSubmtToMeReq.page, orderSubmtToMeReq, orderSubmtToMeReq.sortname, orderSubmtToMeReq.sortorder, UserID, orderSubmtToMeReq.types, orderSubmtToMeReq.Applicator, orderSubmtToMeReq.Customer, orderSubmtToMeReq.Status, orderSubmtToMeReq.BeginDate, orderSubmtToMeReq.EndDate, ViewCustom: viewCustom, ViewSales: viewSales, out rowCount);
 
             List<SaleDeptSubToMe> saleDeptSubToMes = dt.Tolist<SaleDeptSubToMe>();
             foreach (SaleDeptSubToMe item in saleDeptSubToMes)
