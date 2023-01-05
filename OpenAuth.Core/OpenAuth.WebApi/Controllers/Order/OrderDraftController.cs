e6cb9000e8942410bc3dbf57eb647562cc4a5b5d
xiarenchun <neware.com.cn@DELL-5000>
2023-01-05 12:25:11 +0800

    fix: 物料结构优化

--- OpenAuth.Core/OpenAuth.WebApi/Controllers/Order/OrderDraftController.cs
+++ OpenAuth.Core/OpenAuth.WebApi/Controllers/Order/OrderDraftController.cs
@@ -1993,7 +1993,27 @@ namespace OpenAuth.WebApi.Controllers.Order
             var result = new TableData();
             var UserID = _serviceBaseApp.GetUserNaspId();
             var SboID = _serviceBaseApp.GetUserNaspSboID(UserID);
-            result.Data = _serviceSaleOrderApp.SelectCopyItemAllView(out rowCount, model, SboID, UserID);
+            DataTable dt = _serviceSaleOrderApp.SelectCopyItemAllView(out rowCount, model, SboID, UserID);
+            List<CopyItemMsg> copyItemMsgs = dt.Tolist<CopyItemMsg>();
+            foreach (CopyItemMsg item in copyItemMsgs)
+            {
+                if (item.item_cfg_id != 0)
+                {
+                    //将子物料添加为主物料的child
+                    List<SaleItemDtoChild> saleItemDtoChildren = _serviceSaleOrderApp.GetItemConfigList(item.item_cfg_id.ToString(), item.whsCode);
+                    item.childBillSalesDetails = new List<CopyItemMsg>();
+                    foreach (SaleItemDtoChild itemChild in saleItemDtoChildren)
+                    {
+                        CopyItemMsg orderItemInfo = copyItemMsgs.Where(r => r.itemCode == itemChild.ItemCode).FirstOrDefault();
+                        if (orderItemInfo != null)
+                        {
+                            item.childBillSalesDetails.Add(orderItemInfo);
+                        }
+                    }
+                }
+            }
+
+            result.Data = copyItemMsgs;
             result.Count = rowCount;
             return result;
 
